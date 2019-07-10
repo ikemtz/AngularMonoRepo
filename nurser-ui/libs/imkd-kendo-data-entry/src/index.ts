bb817997a5ada18aaff9a768b389eb5027c63883
Isaac Martinez <isaac.martinez@mastercorp.com>
2019-07-09 20:01:14 -0400

    exposing updateStateCollection

--- nurser-ui/libs/imkd-kendo-data-entry/src/index.ts
+++ nurser-ui/libs/imkd-kendo-data-entry/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/data-entry.module';
 export * from './lib/data-entry-dialog/data-entry-dialog.component';
 export * from './lib/base-data-entry.component';
 export * from './lib/data-entry-facade';
+export * from './lib/reducer-logic';
--- nurser-ui/libs/imkd-kendo-data-entry/src/public_api.ts
+++ nurser-ui/libs/imkd-kendo-data-entry/src/public_api.ts
@@ -2,3 +2,4 @@ export { DataEntryDialogModule } from './lib/data-entry.module';
 export { DataEntryDialogComponent } from './lib/data-entry-dialog/data-entry-dialog.component';
 export { BaseDataEntryComponent } from './lib/base-data-entry.component';
 export { DataEntryFacade } from './lib/data-entry-facade';
+export { updateStateCollection } from './lib/reducer-logic';
