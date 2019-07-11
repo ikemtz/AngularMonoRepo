4389bc939355ebe2208e29bad5ed9098477e58df
Isaac Martinez <isaac.martinez@mastercorp.com>
2019-07-11 13:53:24 -0400

    attempt to fix imng-auth0-oidc

--- nurser-ui/libs/imao-auth0-oidc/src/lib/auth-guard.ts
+++ nurser-ui/libs/imao-auth0-oidc/src/lib/auth-guard.ts
@@ -1,36 +1,19 @@
-import {
-  Router,
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot
-} from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { switchMap, first } from 'rxjs/operators';
 import { OidcFacade } from './facades/oidc.facade';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private oidcFacade: OidcFacade) { }
+  constructor(private router: Router, private oidcFacade: OidcFacade) {}
 
-  public canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean> | boolean {
+  public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
     return this.oidcFacade.waitForAuthenticationLoaded().pipe(
       switchMap(loading => {
         return this.oidcFacade.identity$.pipe(
           first(),
-          switchMap(user => {
-            if (user && !user.expired) {
-              return of(true);
-            } else {
-              this.router.navigate(['/']);
-              return of(false);
-            }
-          })
+          switchMap(user => of(user && !user.expired))
         );
       })
     );
