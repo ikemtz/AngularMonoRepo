2e76c6b8da5f1f8d57d557e37108a26e9002aa1f
Isaac Martinez <isaac.martinez@mastercorp.com>
2019-07-22 10:01:53 -0400

    Adding BaseWebApiService to data-entry package

--- nurser-ui/libs/imkd-kendo-data-entry/src/index.ts
+++ nurser-ui/libs/imkd-kendo-data-entry/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/data-entry-dialog/data-entry-dialog.component';
 export * from './lib/base-data-entry.component';
 export * from './lib/data-entry-facade';
 export * from './lib/reducer-collection-logic';
+export * from './lib/base-webapi.service';
