e1e8cca144a2e96c08fe92efafdba51f71f27c07
Isaac Martinez <isaac.martinez@mastercorp.com>
2019-09-10 14:45:25 -0400

    adding imng-nrsrx-utils

---
+++ nurser-ui/libs/imng-nrsrx-utils/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'imng-nrsrx-utils',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/libs/imng-nrsrx-utils',
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js'
+  ]
+};
---
+++ nurser-ui/libs/imng-nrsrx-utils/src/lib/nrsrx-api-client.ts
@@ -0,0 +1,20 @@
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export abstract class NrsrxBaseApiClientService<T extends { id: string }> {
+  public abstract url: string;
+  constructor(private http: HttpClient) { }
+
+  public put(payload: T): Observable<T> {
+    return this.http.put<T>(this.url, payload);
+  }
+
+  public post(payload: T): Observable<T> {
+    return this.http.post<T>(`${this.url}?id=${payload.id}`, payload);
+  }
+
+  public delete(id: string): Observable<T> {
+    return this.http.delete<T>(`${this.url}?id=${id}`);
+  }
+}
