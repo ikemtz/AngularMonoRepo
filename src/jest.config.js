65d27c2506a6cd5f6f03368ce8b331d9c2139194
Isaac Martinez <isaac.martinez@mastercorp.com>
2019-10-20 18:30:48 -0400

    Fixing cobertura coverage issue

--- src/jest.config.js
+++ src/jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   collectCoverage: true,
-  coverageReporters: ['html', 'lcov']
+  coverageReporters: ['html', 'lcov', 'cobertura']
 };
