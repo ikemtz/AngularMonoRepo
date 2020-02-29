7d7e71d121138fbcc88031dc46357e1317f2aa57
Isaac Martinez <isaac.martinez@mastercorp.com>
2020-02-29 14:33:45 -0500

    Adding release process for AzConfig

--- src/apps/imng-az-func-configr/src/app/app.component.spec.ts
+++ src/apps/imng-az-func-configr/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
@@ -14,16 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'imng-az-func-configr'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('imng-az-func-configr');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to imng-az-func-configr!');
+    expect(compiled.querySelector('h2').textContent).toContain('Azure Function Configuration Converter');
   });
 });
