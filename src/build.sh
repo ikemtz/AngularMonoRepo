b35e6496b2d5ee7a4173aa3216429c41d307b553
Isaac Martinez <isaac.martinez@mastercorp.com>
2020-03-02 16:50:14 -0500

    Fixing versioner script

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #
+echo ** NPM Package Versioner Script **
+ls ./libs/**/package.json
+echo BuildNumber $1
 buildNumber=$1
 value='s/\"version\"\: \"[0-9.]*\"/"version\"\: \"X\"/m'
 value="${value/X/$buildNumber}"\
 
-sed -i "$value" **/package.json
+$(sed -i "$value" ./libs/**/package.json)
