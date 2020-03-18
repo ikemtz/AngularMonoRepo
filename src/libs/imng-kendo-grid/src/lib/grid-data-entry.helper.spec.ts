c39f13f7505414412e949e734c57cd02b6b10b32
Isaac Martinez <isaac.martinez@mastercorp.com>
2020-03-18 14:58:54 -0400

    Increasing code coverage

--- src/libs/imng-kendo-grid/src/lib/grid-data-entry.helper.spec.ts
+++ src/libs/imng-kendo-grid/src/lib/grid-data-entry.helper.spec.ts
@@ -1,13 +1,15 @@
-import { TestBed } from '@angular/core/testing';
 import { GridDataEntryHelper } from './grid-data-entry.helper';
 import { FormGroup, FormControl } from '@angular/forms';
 import { readFirst } from '@nrwl/angular/testing';
 
-describe('GridDataEntryHelper<>', () => {
-  const gridComponentMock: any = {
+function gridComponentMockFac(): any {
+  return {
     closeRow: jest.fn(() => {}),
     addRow: jest.fn(() => {}),
+    editRow: jest.fn(() => {}),
   };
+}
+describe('GridDataEntryHelper<>', () => {
   it('should report invalid if gridData is empty ', async done => {
     try {
       const gridHelper = new GridDataEntryHelper(
@@ -27,6 +29,7 @@ describe('GridDataEntryHelper<>', () => {
       done.fail(x);
     }
   });
+
   it('should report invalid if gridData is inEditMode ', async done => {
     try {
       const gridHelper = new GridDataEntryHelper(
@@ -36,7 +39,7 @@ describe('GridDataEntryHelper<>', () => {
           }),
         [{ id: '😎😎' }],
       );
-      gridHelper.addHandler({ sender: gridComponentMock, dataItem: {} } as any);
+      gridHelper.addHandler({ sender: gridComponentMockFac(), dataItem: {} } as any);
       expect(await readFirst(gridHelper.isValid$)).toBe(false);
       expect(gridHelper.isValid).toBe(false);
       expect(gridHelper.isInEditMode).toBe(true);
@@ -79,11 +82,64 @@ describe('GridDataEntryHelper<>', () => {
         dataItem: {},
         isNew: false,
         rowIndex: 1,
-        sender: gridComponentMock,
+        sender: gridComponentMockFac(),
       });
       const result = await readFirst(gridHelper.gridData$);
       expect(result).toMatchSnapshot();
       expect((result[1] as any).test).toBe('👍');
+      done();
+    } catch (x) {
+      done.fail(x);
+    }
+  });
+
+  it('should handle saving new records ', async done => {
+    try {
+      const formGroupFac = () =>
+        new FormGroup({
+          id: new FormControl('🐂🤏'),
+          test: new FormControl('👍'),
+        });
+      const gridHelper = new GridDataEntryHelper(formGroupFac, [{ id: '💩' }, { id: '🐂' }, { id: '🥜' }]);
+
+      gridHelper.saveHandler({
+        formGroup: formGroupFac(),
+        dataItem: {},
+        isNew: true,
+        rowIndex: 4,
+        sender: gridComponentMockFac(),
+      });
+      const result = await readFirst(gridHelper.gridData$);
+      expect(result).toMatchSnapshot();
+      const newRec: any = result[3];
+      expect(newRec.id).toBeNull();
+      expect(newRec.test).toBe('👍');
+      done();
+    } catch (x) {
+      done.fail(x);
+    }
+  });
+
+  it('should editHandler', async done => {
+    try {
+      const formGroupFac = () =>
+        new FormGroup({
+          id: new FormControl('🐂🤏'),
+          test: new FormControl('👍'),
+        });
+      const gridHelper = new GridDataEntryHelper(formGroupFac, [{ id: '💩' }, { id: '🐂' }, { id: '🥜' }]);
+      const gridComponentMock = gridComponentMockFac();
+      gridHelper.editHandler({
+        dataItem: {},
+        isNew: true,
+        rowIndex: 4,
+        sender: gridComponentMock,
+      });
+      expect(gridHelper.gridFormGroup).toBeTruthy();
+      expect(gridHelper.gridFormGroup.value).toMatchSnapshot();
+      expect(gridComponentMock.editRow).toBeCalledTimes(1);
+      expect(gridComponentMock.closeRow).toBeCalledTimes(1);
+      expect(gridComponentMock.closeRow).toHaveBeenNthCalledWith(1, 4);
       done();
     } catch (x) {
       done.fail(x);
--- src/libs/imng-kendo-grid/src/lib/imng-editable-data-grid.directive.spec.ts
+++ src/libs/imng-kendo-grid/src/lib/imng-editable-data-grid.directive.spec.ts
@@ -1,10 +1,27 @@
 import { ImngEditableDataGridDirective } from './imng-editable-data-grid.directive';
+import { of } from 'rxjs';
+
+const gridComponent: any = {
+  edit: of({}),
+  cancel: of({}),
+  save: of({}),
+  remove: of({}),
+  add: of({}),
+};
 
 describe('ImngEditableDataGridDirective', () => {
   it('should create an instance', () => {
-    const gridComponent: any = {};
     const changeDetectorRef: any = {};
     const directive = new ImngEditableDataGridDirective(gridComponent, changeDetectorRef);
     expect(directive).toBeTruthy();
+    directive.ngOnInit();
+  });
+
+  it('should destroy an instance', () => {
+    const changeDetectorRef: any = {};
+    const directive = new ImngEditableDataGridDirective(gridComponent, changeDetectorRef);
+    expect(directive).toBeTruthy();
+    directive.ngOnInit();
+    directive.ngOnDestroy();
   });
 });
