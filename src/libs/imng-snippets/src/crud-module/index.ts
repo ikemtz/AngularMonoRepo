596248aec6f6e7dfcbc745abc59d0b89b8ce9a2a
IkeMtz <ikemtz@outlook.com>
2020-05-25 12:12:51 -0400

    Scaffolding complete

--- src/libs/imng-snippets/src/crud-module/index.ts
+++ src/libs/imng-snippets/src/crud-module/index.ts
@@ -3,7 +3,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function imngSnippets(_options: any): Rule {
+export function crudModule(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
--- src/libs/imng-snippets/src/crud-module/index_spec.ts
+++ src/libs/imng-snippets/src/crud-module/index_spec.ts
@@ -6,10 +6,10 @@ import * as path from 'path';
 const collectionPath = path.join(__dirname, '../collection.json');
 
 
-describe('imng-snippets', () => {
+describe('crud-module', () => {
   it('works', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('imng-snippets', {}, Tree.empty());
+    const tree = runner.runSchematic('crud-module', {}, Tree.empty());
 
     expect(tree.files).toEqual([]);
   });
