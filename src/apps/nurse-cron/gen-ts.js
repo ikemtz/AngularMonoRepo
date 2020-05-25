fd6eda78ec3bc755ba7b024442ca97ca7af1d325
IkeMtz <ikemtz@outlook.com>
2020-05-25 15:21:14 -0400

    Fixing CiCd script

--- src/apps/nurse-cron/gen-ts.js
+++ src/apps/nurse-cron/gen-ts.js
@@ -1,29 +1,37 @@
 const generator = require('openapi-ts-generator');
 
-async function genTypeScriptModels() {
+async function generateTsModels() {
   //Employees
-  console.log("** Employees OData Models **");
-  await generator.generateTsModels(
-    'https://im-wa-empo-nrsr.azurewebsites.net/swagger/v1/swagger.json',
-    './apps/nurse-cron/src/app/modules/models/emp-odata/'
-  );
-  console.log("** Employees WebApi Models **");
-  await generator.generateTsModels(
-    'https://im-wa-empa-nrsr.azurewebsites.net/swagger/v1/swagger.json',
-    './apps/nurse-cron/src/app/modules/models/emp-api/'
-  );
-
+  console.log('** Employees OData Models **');
+  await generator.generateTsModels({
+    openApiJsonUrl: 'https://im-wa-empo-nrsr.azurewebsites.net/swagger/v1/swagger.json',
+    outputPath: './apps/nurse-cron/src/app/modules/models/emp-odata/',
+    typeFilterCallBack: (val, i, arr) => !val.name.endsWith('ODataEnvelope'),
+    valuePropertyTypeFilterCallBack: (val, i, arr) =>
+      !val.name.startsWith('created') && !val.name.startsWith('updated'),
+  });
+  console.log('** Employees WebApi Models **');
+  await generator.generateTsModels({
+    openApiJsonUrl: 'https://im-wa-empa-nrsr.azurewebsites.net/swagger/v1/swagger.json',
+    outputPath: './apps/nurse-cron/src/app/modules/models/emp-api/',
+    valuePropertyTypeFilterCallBack: (val, i, arr) =>
+      !val.name.startsWith('created') && !val.name.startsWith('updated'),
+  });
   //Certifications
-  console.log("** Certifications OData Models **");
-  await generator.generateTsModels(
-    'https://im-wa-crto-nrsr.azurewebsites.net/swagger/v1/swagger.json',
-    './apps/nurse-cron/src/app/modules/models/cert-odata-models/'
-  );
-  console.log("** Certifications WebApi Models **");
-  await generator.generateTsModels(
-    'https://im-wa-crta-nrsr.azurewebsites.net/swagger/v1/swagger.json',
-    './apps/nurse-cron/src/app/modules/models/cert-api-models/'
-  );
+  console.log('** Certifications OData Models **');
+  await generator.generateTsModels({
+    openApiJsonUrl: 'https://im-wa-crto-nrsr.azurewebsites.net/swagger/v1/swagger.json',
+    outputPath: './apps/nurse-cron/src/app/modules/models/cert-odata-models/',
+    valuePropertyTypeFilterCallBack: (val, i, arr) =>
+      !val.name.startsWith('created') && !val.name.startsWith('updated'),
+  });
+  console.log('** Certifications WebApi Models **');
+  await generator.generateTsModels({
+    openApiJsonUrl: 'https://im-wa-crta-nrsr.azurewebsites.net/swagger/v1/swagger.json',
+    outputPath: './apps/nurse-cron/src/app/modules/models/cert-api-models/',
+    valuePropertyTypeFilterCallBack: (val, i, arr) =>
+      !val.name.startsWith('created') && !val.name.startsWith('updated'),
+  });
 }
 
 genTypeScriptModels();
