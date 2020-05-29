ce761096f85a13157322af74996cf384e858e699
IkeMtz <ikemtz@outlook.com>
2020-05-29 18:57:29 -0400

    Fixing broken build

--- src/versioneer.sh
+++ src/versioneer.sh
@@ -2,10 +2,12 @@
 #
 echo '** NPM Package Versioneer Script **'
 ls ./libs/imng-*/package.json
+ls ./apps/*/src/environments/environment*ts
+
 echo BuildNumber $1
 buildNumber=$1
 value='s/\"?version\"?\: \"[0-9.]*\"/"version\"\: \"X\"/m'
 value="${value/X/$buildNumber}"\
 
 $(sed -i "$value" ./libs/imng-*/package.json)
-$(sed -i "$value" ./apps/**/environments/environment*ts)
+$(sed -i "$value" ./apps/*/src/environments/environment*ts)
