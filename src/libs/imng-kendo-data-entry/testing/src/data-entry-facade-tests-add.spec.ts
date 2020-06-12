3e44e59b7ddb26a6d9864e641c13ab80a09b725c
IkeMtz <ikemtz@outlook.com>
2020-06-12 13:49:43 -0400

    Increasing code coverage on kendo-data-entry

---
+++ src/libs/imng-kendo-data-entry/testing/src/data-entry-facade-tests-add.spec.ts
@@ -0,0 +1,28 @@
+import { testAddSetAndClearCurrentEntity } from '.';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { isTruthy } from 'imng-ngrx-utils';
+import { testEditSetAndClearCurrentEntity } from './data-entry-facade-tests';
+
+describe('TestAddSetAndClearCurrentEntity', () => {
+    it('New Entity Set And Clear CurrentEntity', async done =>
+        testAddSetAndClearCurrentEntity<TestFacade>(done, new TestFacade()));
+});
+
+describe('TestEditSetAndClearCurrentEntity', () => {
+    it('Existing Entity Set And Clear CurrentEntity', async done =>
+        testEditSetAndClearCurrentEntity<TestFacade>(done, new TestFacade()));
+});
+class TestFacade {
+    entity$ = new BehaviorSubject<{ id: string, name: string; }>(null);
+    currentEntity$ = this.entity$.asObservable();
+    isEditActive$ = this.currentEntity$.pipe(map(m => m && isTruthy(m.id)));
+    isNewActive$ = this.entity$.pipe(map(m => m && !isTruthy(m.id)));
+    setCurrentEntity(entity: { id: string, name: string; }, parentEntity?: unknown): void {
+        this.entity$.next(entity);
+    }
+    clearCurrentEntity(): void {
+        this.entity$.next(null);
+    }
+
+}
