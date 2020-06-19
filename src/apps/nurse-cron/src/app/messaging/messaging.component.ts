3c373ef815f623dc28179dadc38e4ed97d5e2dc6
IkeMtz <ikemtz@outlook.com>
2020-06-19 06:16:41 -0400

    Working client connected to proxy

--- src/apps/nurse-cron/src/app/messaging/messaging.component.ts
+++ src/apps/nurse-cron/src/app/messaging/messaging.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SignalrFacade, signalrActions } from 'imng-signalr-ngrx';
-import { take, filter } from 'rxjs/operators';
+import { SignalrFacade, signalrActions, ISignalrMessage } from 'imng-signalr-ngrx';
+import { take, filter, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'nrcrn-messaging',
@@ -8,13 +9,19 @@ import { take, filter } from 'rxjs/operators';
   styleUrls: ['./messaging.component.scss']
 })
 export class MessagingComponent implements OnInit {
-
+  public lastMessage$: Observable<ISignalrMessage<string>>;
   constructor(private readonly signalrFacade: SignalrFacade) { }
 
   ngOnInit(): void {
-    this.signalrFacade.isConnected$.pipe(filter(t => t), take(1));
+    this.signalrFacade.isConnected$.pipe(
+      filter(t => t),
+      take(1),
+      tap(t => this.signalrFacade.dispatchAction(signalrActions.sendMessage({
+        methodName: 'SendMessage',
+        data: 'Signed In'
+      }))));
+    this.lastMessage$ = this.signalrFacade.lastReceivedMessage$;
     this.signalrFacade.dispatchAction(signalrActions.connect());
-
   }
 
 
