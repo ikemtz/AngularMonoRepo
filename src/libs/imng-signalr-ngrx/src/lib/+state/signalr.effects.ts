63a44a740b0800663cfe5b48466ab60f55b6ed02
IkeMtz <ikemtz@outlook.com>
2020-06-19 04:09:04 -0400

    signalr lib fixes

--- src/libs/imng-signalr-ngrx/src/lib/+state/signalr.effects.ts
+++ src/libs/imng-signalr-ngrx/src/lib/+state/signalr.effects.ts
@@ -1,11 +1,9 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable, } from '@angular/core';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { map } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
 import { signalrActions } from './signalr.actions';
 import { HubConnectionInjectorService } from '../services/hub-connection-injector.service';
-import { SignalrPartialState } from './signalr.reducer';
-import { SIGNALR_CONFIG, ISignalrConfiguration } from '../models/signalr.configuration';
+
 @Injectable()
 export class SignalrEffects {
 
@@ -21,18 +19,7 @@ export class SignalrEffects {
   ), { dispatch: false });
 
   constructor(private readonly actions$: Actions,
-    private readonly store$: Store<SignalrPartialState>,
     private readonly hubConnectionInjectorService: HubConnectionInjectorService,
-    @Inject(SIGNALR_CONFIG) signalrConfiguration: ISignalrConfiguration
   ) {
-
-    this.hubConnectionInjectorService.hubConnection.onclose(async () => {
-      this.store$.dispatch(signalrActions.connect());
-    });
-    signalrConfiguration.clientMethods.forEach(clientMethod => {
-      this.hubConnectionInjectorService.hubConnection.on(clientMethod,
-        data => this.store$.dispatch(signalrActions.receivedMessage({ methodName: clientMethod, data: data }))
-      );
-    });
   }
 }
--- src/libs/imng-signalr-ngrx/src/lib/services/hub-connection-injector.service.ts
+++ src/libs/imng-signalr-ngrx/src/lib/services/hub-connection-injector.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, Inject, OnDestroy } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { ISignalrConfiguration, SIGNALR_CONFIG } from '../models/signalr.configuration';
-import { OidcFacade } from 'imng-auth0-oidc';
 import { Subscription } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, filter } from 'rxjs/operators';
+import { signalrActions } from '../+state/signalr.actions';
+import { Store } from '@ngrx/store';
+import * as fromSignalr from '../+state/signalr.reducer';
+import { OidcFacade } from 'imng-auth0-oidc';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +15,25 @@ export class HubConnectionInjectorService implements OnDestroy {
   private readonly subscriptions: Subscription[] = [];
   public hubConnection: HubConnection;
 
-  constructor(@Inject(SIGNALR_CONFIG) signalrConfiguration: ISignalrConfiguration, oidcFacade: OidcFacade) {
+  constructor(@Inject(SIGNALR_CONFIG) signalrConfiguration: ISignalrConfiguration, private readonly store$: Store<fromSignalr.SignalrPartialState>, oidcFacade: OidcFacade) {
     this.subscriptions.push(oidcFacade.accessToken$.pipe(
+      filter(accessToken => !!accessToken),
       tap(accessToken => {
         this.hubConnection = new HubConnectionBuilder()
           .withUrl(signalrConfiguration.hostUrl, { accessTokenFactory: () => accessToken })
           .configureLogging(signalrConfiguration.logLevel)
           .withAutomaticReconnect()
           .build();
+
+        this.hubConnection.onclose(async () => {
+          this.store$.dispatch(signalrActions.connect());
+        });
+
+        signalrConfiguration.clientMethods.forEach(clientMethod => {
+          this.hubConnection.on(clientMethod,
+            data => this.store$.dispatch(signalrActions.receivedMessage({ methodName: clientMethod, data: data }))
+          );
+        });
       })).subscribe());
 
   }
