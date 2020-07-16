dc732f56de7e49c7b6431c1478fc7960334940da
IkeMtz <ikemtz@outlook.com>
2020-07-15 22:34:28 -0400

    Fixing code smells

--- src/libs/imng-auth0-oidc/src/lib/models/auth0-config.ts
+++ src/libs/imng-auth0-oidc/src/lib/models/auth0-config.ts
@@ -4,8 +4,29 @@ export interface Auth0Config {
   audience: string;
   client_id: string;
   authority: string;
-  scope?: "openid" | "profile" | "offline_access" | "name" | "given_name" | "family_name" | "nickname" | "email" | "email_verified" | "picture" | "created_at" | "identities" | "phone" | "address" | string;
-  response_type?: "code" | "token" | "id_token" | "code token" | "code id_token" | "token id_token" | "code token id_token" | string;
+  scope?: "openid" |
+  "profile" |
+  "offline_access" |
+  "name" |
+  "given_name" |
+  "family_name" |
+  "nickname" |
+  "email" |
+  "email_verified" |
+  "picture" |
+  "created_at" |
+  "identities" |
+  "phone" |
+  "address" |
+  string;
+  response_type?: "code" |
+  "token" |
+  "id_token" |
+  "code token" |
+  "code id_token" |
+  "token id_token" |
+  "code token id_token" |
+  string;
   automaticSilentRenew?: boolean;
 }
 export const AUTH0_CONFIG = new InjectionToken<Auth0Config>('auth0-config');
