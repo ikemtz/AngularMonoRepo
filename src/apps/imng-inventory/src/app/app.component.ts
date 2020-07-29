620e1f6e8c1bc8acda85ac81feb322d11d10d22f
IkeMtz <ikemtz@outlook.com>
2020-07-29 07:41:51 -0400

    Minor fixes to auth0 library

--- src/apps/imng-inventory/src/app/app.component.ts
+++ src/apps/imng-inventory/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Quagga from 'quagga';
 
 @Component({
   selector: 'imng-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'imng-inventory';
+export class AppComponent implements AfterViewInit {
+  public errorMessage: string;
+
+
+  ngAfterViewInit(): void {
+    if (!navigator.mediaDevices || typeof navigator.mediaDevices.getUserMedia !== 'function') {
+      this.errorMessage = 'getUserMedia is not supported';
+      return;
+    }
+
+    Quagga.init({
+      inputStream: {
+        constraints: {
+          facingMode: 'environment'
+        },
+        area: { // defines rectangle of the detection/localization area
+          top: '40%',    // top offset
+          right: '0%',  // right offset
+          left: '0%',   // left offset
+          bottom: '40%'  // bottom offset
+        },
+      },
+      decoder: {
+        readers: ['ean_reader']
+      },
+    },
+      (err) => {
+        if (err) {
+          this.errorMessage = `QuaggaJS could not be initialized, err: ${err}`;
+        } else {
+          Quagga.start();
+          Quagga.onDetected((res) => {
+            this.onBarcodeScanned(res.codeResult.code);
+          });
+        }
+      });
+  }
+
+  onBarcodeScanned(code: string) {
+    console.log(code);
+  }
 }
