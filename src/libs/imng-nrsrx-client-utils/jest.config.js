09ac4bad22feed1b3c62631bb136b87b15737627
IkeMtz <ikemtz@outlook.com>
2020-07-30 00:11:30 -0400

    updating jest configuration

--- src/libs/imng-nrsrx-client-utils/jest.config.js
+++ src/libs/imng-nrsrx-client-utils/jest.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   name: 'imng-nrsrx-client-utils',
   preset: '../../jest.config.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
   coverageDirectory: '../../coverage/libs/imng-nrsrx-client-utils',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
