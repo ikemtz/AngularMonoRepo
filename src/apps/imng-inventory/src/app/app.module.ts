bdf7b8f78e3a498f4b39101f09b544a059472845
IkeMtz <ikemtz@outlook.com>
2020-08-02 11:03:21 -0400

    Adding functional barcode scanner

--- src/apps/imng-inventory/src/app/app.module.ts
+++ src/apps/imng-inventory/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
+import { BarecodeScannerLivestreamModule } from 'ngx-barcode-scanner';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' })],
+  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' }), BarecodeScannerLivestreamModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
