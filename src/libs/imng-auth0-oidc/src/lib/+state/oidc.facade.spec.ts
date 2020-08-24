55715f548ec20d3ad76f2752f4658d06c12869a3
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2020-08-24 13:46:52 -0400

    Adding transformation to odata library (#111)
    * Adding support for CanActivateChild
    
    * Resolving code smell
    
    * Adding userInfo functionality in auth lib
    
    * Fixing typo
    
    * Fixing build break
    
    * Fixing oidc unit tests
    
    * Increasing code coverage
    
    * Adding functional barcode scanner
    
    * resolving sonar cloud warnings in build logs
    
    * Fixing ci/cd script
    
    * Updating schematics
    
    * Updating libs YAML
    
    * Removing SSL valiatoin on swagger doc pull
    
    * Adding support for non-http swagger endpoints
    
    * Updating schematics build YAML
    
    * Matking http match non-case sensitive
    
    * Cleaning up environment variable configuration
    
    * Good stopping point
    
    * Adding imng-ngrx-idle
    
    * Fixing imng-ngrx-idle prod build
    
    * Addressig code smell and code coverage
    
    * Adding imng-ngrx-idle test scripts
    
    * Fixing schematic templates with file based generation
    
    * Adding ImngArrayDirective
    
    * Increasing code coverage
    
    * quick code cleanup
    
    * Fixing accessTokenExpired handling
    
    * Switching KendoArray grid storage to be reactive
    
    * Fixing unit tests
    
    * Addressing ngrx deep imports warning
    
    * Fixing page size issue on imngArrayGrid
    
    * Adding filtering capabilities to imngArrayGrid directive
    
    * Addressing timing issue on datastateChange vs filterChange
    
    * excluding testing folders from code coverage
    
    * last fix on filtering
    
    * Updating NGRX Idle
    
    * Cleaning up oidc ngrx effects
    
    * Fixing unit test and increasing code coverage
    
    * Fixing unit tests
    
    * Increasing code coverage
    
    * Adding transformation to odata library

--- src/libs/imng-auth0-oidc/src/lib/+state/oidc.facade.spec.ts
+++ src/libs/imng-auth0-oidc/src/lib/+state/oidc.facade.spec.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, InjectionToken } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule, Store } from '@ngrx/store';
@@ -15,7 +15,7 @@ import {
 import * as oidcActions from './oidc.actions';
 import { OidcService } from '../services/oidc.service';
 import { OidcFacade } from './oidc.facade';
-import { OIDC_CONFIG } from '../models/config.model';
+import { OIDC_CONFIG, Config } from '../models/config.model';
 import { of } from 'rxjs';
 import { HttpErrorResponse, HttpClient } from '@angular/common/http';
 import { Auth0Facade } from './auth0.facade';
@@ -29,6 +29,7 @@ describe('OidcFacade', () => {
   let auth0Facade: Auth0Facade;
   let store: Store<TestSchema>;
   let service: OidcService;
+  let oidcConfig: Config;
 
   beforeEach(() => { });
 
@@ -60,6 +61,7 @@ describe('OidcFacade', () => {
       store = TestBed.inject(Store);
       facade = TestBed.inject(OidcFacade);
       service = TestBed.inject(OidcService);
+      oidcConfig = TestBed.inject(OIDC_CONFIG);
       auth0Facade = TestBed.inject(Auth0Facade);
     });
 
@@ -73,6 +75,40 @@ describe('OidcFacade', () => {
         done.fail(err);
       }
     });
+
+    it('should getOidcUser with no metaData', async done => {
+      try {
+        oidcConfig.getUserMetadata = false;
+        const oidcUser = {
+          access_token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik16azRRa1l4TVRCRE4wRXpSVFZGTXpJNE4wUXdRelJETVVSRU5UZ3pOVVpCTnpWRFFqbEdPQSJ9.eyJpc3MiOiJodHRwczovL25yc3J4LWRlbW8uYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTExMzAyODgxMjE2NzkxNTQ2NjI2IiwiYXVkIjpbIklNLU51cnNlciIsImh0dHBzOi8vbnJzcngtZGVtby5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNTkwMzg0NzQzLCJleHAiOjE1OTAzOTE5NDMsImF6cCI6Ikg4NjdSRGlFS0Q2V1B2cWVGRm5xVTJtMmc2MEY5Rkg3Iiwic2NvcGUiOiJvcGVuaWQgZW1haWwifQ.ukutcSkeYu_utBtV_pTNEGke6oQR-MYIV8SeA5KQLUKvrRxz7bzgi6FrjnSv8Tvw32gfCtanPbNm12VIqaaf8j0ER28RMIDP6jzsAJkScf4DDKCYGrJJNm1KCclSl2qNADqQUil59Ce98xtsAsTkkwAHenoOljYI90RFWQYJHWQkI_4IrClWcfyb33HKwCpz2wn3whbOFVDHAcubHE9NadptBzlt01cffFQEFWVyYZX0UaVqHCGwTNmSrGr_wlAfnuQRm2IhpKXaAZ3WKjDDtp85ZfAgHx-frGV6uVafmoMP1rAHnt37f8SLMu93hStunrsnJzbygzm_m08qg54l9A',
+          expiresAt: '2020-05-25T07:32:25.941Z',
+          id_token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik16azRRa1l4TVRCRE4wRXpSVFZGTXpJNE4wUXdRelJETVVSRU5UZ3pOVVpCTnpWRFFqbEdPQSJ9.eyJlbWFpbCI6ImlrZW10ekBvdXRsb29rLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL25yc3J4LWRlbW8uYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTExMzAyODgxMjE2NzkxNTQ2NjI2IiwiYXVkIjoiSDg2N1JEaUVLRDZXUHZxZUZGbnFVMm0yZzYwRjlGSDciLCJpYXQiOjE1OTAzODQ3NDMsImV4cCI6MTU5MDQyMDc0MywiYXRfaGFzaCI6InVFU1FxWU1UZXNwbFlZSWVBWl8wRFEiLCJub25jZSI6IjNkc3M0bmZPc2ZBbnNDREtJejE5bVY5dGhnd0h2NVJSIn0.pMe9aLarMI03ygDzEl-G96DH1gQTHB7tRO-3LiHh_WnHObVccmcUDpWXqt6N9FnRIokeBsfi-qerOOfJ06ep91C2w3A3FImcGS8FCBaC9C3MbEuEhqIac2hlKHXLvT8FwaFzxccXaFXMifdiujmsIX1IS3Ec2c8UrOBBbN1-bDXbElf4A9hGH7rY2YC0w774KQMO0-qkz7_x3GkJWuVAW3zPWSBnQ7wLj8Me2Css251TVNWZJKXo3EzxORXcmCYn4noboTW5Bc1M7hYQ4M3JWs5P4ycU_6W2VDoP1g49tZvcSUNLsT-Dn7Ge97Qvymg8gB0hUmigwLKHpZmfivw9gw',
+          userName: 'ikemtz@outlook.com',
+          toStorageString: jest.fn()
+        };
+        const getUserMock = jest.fn(() => of(oidcUser as any));
+        const removeUserMock = jest.fn(() => new Promise<void>(resolve => resolve()));
+        (service as any)._oidcUserManager.getUser = getUserMock;
+        (service as any)._oidcUserManager.removeUser = removeUserMock;
+        facade.getOidcUser();
+        const getResult = await readFirst(facade.identity$);
+        expect(getResult).toMatchSnapshot('identity');
+        expect(await readFirst(facade.waitForAuthenticationLoaded())).toBe(true);
+        expect(await readFirst(facade.permissions$)).toMatchSnapshot('permissions');
+        expect(await readFirst(facade.userMetadata$)).toMatchSnapshot('userMetadata');
+        expect(await readFirst(auth0Facade.permissions$)).toMatchSnapshot('auth0_permissions');
+        expect(await readFirst(auth0Facade.email$)).toMatchSnapshot('email');
+        expect(await readFirst(auth0Facade.profilePicture$)).toMatchSnapshot('profilePicture');
+
+        expect(await readFirst(auth0Facade.hasPermissions(['requiredPermission']))).toBe(false);
+        expect(getUserMock).toBeCalledTimes(1);
+
+        done();
+      } catch (err) {
+        done.fail(err);
+      }
+    });
+
 
     it('should getOidcUser and removeOidcUser', async done => {
       try {
