ed6d81a118b3f75760f071efa67413b73923b700
IkeMtz <ikemtz@outlook.com>
2020-08-29 13:48:28 -0400

    renaming paraeters for doc purposes

--- src/libs/imng-ngrx-utils/src/lib/find-and-modify.ts
+++ src/libs/imng-ngrx-utils/src/lib/find-and-modify.ts
@@ -1,15 +1,16 @@
+
 export function findAndModify<ENTITY extends { id?: number | string; }>(
   data: ENTITY[],
   lookupId: number | string,
-  replacementLogic: (matchingRecord: ENTITY) => void): ENTITY[] {
+  modificationLogic: (matchingRecord: ENTITY) => void): ENTITY[] {
   return [...data.map(m => m.id === lookupId ?
-    applyChanges(m, replacementLogic)
+    applyChanges(m, modificationLogic)
     : m
   )];
 }
 
-function applyChanges<ENTITY>(record: ENTITY, replacementLogic: (matchingRecord: ENTITY) => void): ENTITY {
+function applyChanges<ENTITY>(record: ENTITY, modificationLogic: (matchingRecord: ENTITY) => void): ENTITY {
   const spreadRecord = { ...record };
-  replacementLogic(spreadRecord);
+  modificationLogic(spreadRecord);
   return spreadRecord;
 }
