c583b882fa867cb5e0e24b4ee13c7cb8f59cf8fa
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2020-08-29 14:12:51 -0400

    Renaming to findAndModify (#121)
    * Adding support for CanActivateChild
    
    * Resolving code smell
    
    * Adding userInfo functionality in auth lib
    
    * Fixing typo
    
    * Fixing build break
    
    * Fixing oidc unit tests
    
    * Increasing code coverage
    
    * Adding functional barcode scanner
    
    * resolving sonar cloud warnings in build logs
    
    * Fixing ci/cd script
    
    * Updating schematics
    
    * Updating libs YAML
    
    * Removing SSL valiatoin on swagger doc pull
    
    * Adding support for non-http swagger endpoints
    
    * Updating schematics build YAML
    
    * Matking http match non-case sensitive
    
    * Cleaning up environment variable configuration
    
    * Good stopping point
    
    * Adding imng-ngrx-idle
    
    * Fixing imng-ngrx-idle prod build
    
    * Addressig code smell and code coverage
    
    * Adding imng-ngrx-idle test scripts
    
    * Fixing schematic templates with file based generation
    
    * Adding ImngArrayDirective
    
    * Increasing code coverage
    
    * quick code cleanup
    
    * Fixing accessTokenExpired handling
    
    * Switching KendoArray grid storage to be reactive
    
    * Fixing unit tests
    
    * Addressing ngrx deep imports warning
    
    * Fixing page size issue on imngArrayGrid
    
    * Adding filtering capabilities to imngArrayGrid directive
    
    * Addressing timing issue on datastateChange vs filterChange
    
    * excluding testing folders from code coverage
    
    * last fix on filtering
    
    * Updating NGRX Idle
    
    * Cleaning up oidc ngrx effects
    
    * Fixing unit test and increasing code coverage
    
    * Fixing unit tests
    
    * Increasing code coverage
    
    * Adding transformation to odata library
    
    * Updating ng packages
    
    * Fixing timing issue on kendo array grid
    
    * updating package.json
    
    * npm audit fix
    
    * Updating dependency
    
    * updating package-lock.json
    
    * Adding after ngViewInit
    
    * Removing griddata$ observable from array grid component
    
    * Changing page hooks on array component base
    
    * Setting grid data source in array component
    
    * fixing bug kendo array component
    
    * Kendo Grid Array component fix
    
    * Adding getSubGridData operator
    
    * Fixing sorting issue on Array Grid
    
    * Increasing code coverage
    
    * Increasing code coverage
    
    * Increasing code coverage
    
    * Adding findAndReplace ngrx reducer helper
    
    * Removing snapshot
    
    * Renaming to findAndModify
    
    * renaming paraeters for doc purposes

---
+++ src/libs/imng-ngrx-utils/src/lib/find-and-modify.ts
@@ -0,0 +1,16 @@
+
+export function findAndModify<ENTITY extends { id?: number | string; }>(
+  data: ENTITY[],
+  lookupId: number | string,
+  modificationLogic: (matchingRecord: ENTITY) => void): ENTITY[] {
+  return [...data.map(m => m.id === lookupId ?
+    applyChanges(m, modificationLogic)
+    : m
+  )];
+}
+
+function applyChanges<ENTITY>(record: ENTITY, modificationLogic: (matchingRecord: ENTITY) => void): ENTITY {
+  const spreadRecord = { ...record };
+  modificationLogic(spreadRecord);
+  return spreadRecord;
+}
