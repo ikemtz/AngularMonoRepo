351ce144afa7635fc78d59690abb3dab0920140e
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2020-09-08 12:23:38 -0400

    Adding findById RxJs Operator (#131)
    * Adding support for CanActivateChild
    
    * Resolving code smell
    
    * Adding userInfo functionality in auth lib
    
    * Fixing typo
    
    * Fixing build break
    
    * Fixing oidc unit tests
    
    * Increasing code coverage
    
    * Adding functional barcode scanner
    
    * resolving sonar cloud warnings in build logs
    
    * Fixing ci/cd script
    
    * Updating schematics
    
    * Updating libs YAML
    
    * Removing SSL valiatoin on swagger doc pull
    
    * Adding support for non-http swagger endpoints
    
    * Updating schematics build YAML
    
    * Matking http match non-case sensitive
    
    * Cleaning up environment variable configuration
    
    * Good stopping point
    
    * Adding imng-ngrx-idle
    
    * Fixing imng-ngrx-idle prod build
    
    * Addressig code smell and code coverage
    
    * Adding imng-ngrx-idle test scripts
    
    * Fixing schematic templates with file based generation
    
    * Adding ImngArrayDirective
    
    * Increasing code coverage
    
    * quick code cleanup
    
    * Fixing accessTokenExpired handling
    
    * Switching KendoArray grid storage to be reactive
    
    * Fixing unit tests
    
    * Addressing ngrx deep imports warning
    
    * Fixing page size issue on imngArrayGrid
    
    * Adding filtering capabilities to imngArrayGrid directive
    
    * Addressing timing issue on datastateChange vs filterChange
    
    * excluding testing folders from code coverage
    
    * last fix on filtering
    
    * Updating NGRX Idle
    
    * Cleaning up oidc ngrx effects
    
    * Fixing unit test and increasing code coverage
    
    * Fixing unit tests
    
    * Increasing code coverage
    
    * Adding transformation to odata library
    
    * Updating ng packages
    
    * Fixing timing issue on kendo array grid
    
    * updating package.json
    
    * npm audit fix
    
    * Updating dependency
    
    * updating package-lock.json
    
    * Adding after ngViewInit
    
    * Removing griddata$ observable from array grid component
    
    * Changing page hooks on array component base
    
    * Setting grid data source in array component
    
    * fixing bug kendo array component
    
    * Kendo Grid Array component fix
    
    * Adding getSubGridData operator
    
    * Fixing sorting issue on Array Grid
    
    * Increasing code coverage
    
    * Increasing code coverage
    
    * Increasing code coverage
    
    * Adding findAndReplace ngrx reducer helper
    
    * Removing snapshot
    
    * Renaming to findAndModify
    
    * renaming paraeters for doc purposes
    
    * Adding logout Success component
    
    * Updating logout message
    
    * Adding auto-routing on logout success
    
    * Completing the logout success comp
    
    * Fixing unit test
    
    * Fixing stale loading indicator on odata grids
    
    * Fixing missing loading indicator on grids
    
    * Fixing unit tests
    
    * Updating Build policies and dependencies
    
    * Updating build proceses
    
    * Fixing broken build in schematics
    
    * Updating dependencies
    
    * Updating build process
    
    * Adding findById RxJs Operator
    
    * Increasing code coverage

--- src/libs/imng-kendo-odata/src/lib/odata-rxjs-operators.ts
+++ src/libs/imng-kendo-odata/src/lib/odata-rxjs-operators.ts
@@ -13,7 +13,9 @@ export const mapToExtDataResult = <T>(utcNullableProps: string[] = [], dateNulla
     return result;
   });
 
-export const firstRecord = <T>() => map((result: ODataResult<T>) => (result.data.length > 0 ? result.data[0] : {}) as T);
+export const firstRecord = <T>() => map((result: ODataResult<T>) => (result.data.length > 0 ? result.data[0] : {} as T));
+
+export const findById = <T extends { id: string | number; }>(id: string | number) => map((source: ODataResult<T>) => source.data.find(f => f.id === id) || {} as T);
 
 export function parseDatesInCollection<T>(
   collection: Array<T>,
