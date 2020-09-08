e32ebb02c873e2c444fd92d88347dba7e26a9517
IkeMtz <ikemtz@outlook.com>
2020-09-08 11:14:42 -0400

    Adding findById RxJs Operator

--- src/libs/imng-kendo-odata/src/lib/odata-rxjs-operators.spec.ts
+++ src/libs/imng-kendo-odata/src/lib/odata-rxjs-operators.spec.ts
@@ -1,17 +1,47 @@
 import { of } from 'rxjs';
 import { readFirst } from '@nrwl/angular/testing';
-import { getSubGridData } from './odata-rxjs-operators';
+import { getSubGridData, findById, firstRecord } from './odata-rxjs-operators';
 
 describe('RxJs Operators', () => {
+  const gridData$ = of({
+    data: [
+      { id: 1, subData: [{ val: 'xyz' }, { val: 123 }] },
+      { id: 2, subData: [{ val: 'abc' }, { val: 456 }] },
+    ], total: 5000
+  });
   it('should be created', async done => {
-    const gridData$ = of({
-      data: [
-        { id: 1, subData: [{ val: 'xyz' }, { val: 123 }] },
-        { id: 2, subData: [{ val: 'abc' }, { val: 456 }] },
-      ], total: 5000
-    });
     try {
       const result = await readFirst(gridData$.pipe(getSubGridData(2, x => x.subData)));
+      expect(result).toMatchSnapshot();
+      done();
+    } catch (err) {
+      done.fail(err);
+    }
+  });
+
+  it('should findById', async done => {
+    try {
+      const result = await readFirst(gridData$.pipe(findById(2)));
+      expect(result).toMatchSnapshot();
+      done();
+    } catch (err) {
+      done.fail(err);
+    }
+  });
+
+  it('should not findById', async done => {
+    try {
+      const result = await readFirst(gridData$.pipe(findById(3)));
+      expect(result).toBeUndefined();
+      done();
+    } catch (err) {
+      done.fail(err);
+    }
+  });
+
+  it('should firstRecord', async done => {
+    try {
+      const result = await readFirst(gridData$.pipe(firstRecord()));
       expect(result).toMatchSnapshot();
       done();
     } catch (err) {
