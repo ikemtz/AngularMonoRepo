c27ee21b6c4159ebde77cba5431ec76f9221ff1b
IkeMtz <ikemtz@outlook.com>
2020-09-23 09:43:44 -0400

    Adding date extension methods

--- src/libs/imng-ngrx-utils/src/lib/date-extensions.spec.ts
+++
@@ -1,8 +0,0 @@
-import './date-extensions';
-
-describe('toShortDateString', () => {
-  it('to handle 1/1/2020', () => {
-    const dt = new Date('1/1/2020');
-    expect(dt.toShortDateString()).toBe(false);
-  });
-});
--- src/libs/imng-nrsrx-client-utils/src/lib/date.helper.ts
+++ src/libs/imng-nrsrx-client-utils/src/lib/date.helper.ts
@@ -1,7 +1,10 @@
 export class DateHelper {
-  public static readonly millisecsInDay = 86400000;
   public static weekOfYear(dt: Date): number {
-    const onejan = new Date(dt.getFullYear(), 0, 1);
-    return Math.ceil(((dt.getTime() - onejan.getTime()) / DateHelper.millisecsInDay + onejan.getDay() + 1) / 7);
+    return dt.weekOfYear();
   }
+
+  // tslint:disable-next-line: space-before-function-paren
+  public static toShortDateString(dt: Date): string {
+    return dt.toShortDateString();
+  };
 }
