8a5a4e12e716aadf6ed8054dbad3a4a6d9b6d42b
IkeMtz <ikemtz@outlook.com>
2021-01-14 18:25:20 -0500

    Addressing code smells

--- src/libs/imng-nrsrx-client-utils/src/lib/date.extensions.ts
+++ src/libs/imng-nrsrx-client-utils/src/lib/date.extensions.ts
@@ -10,9 +10,10 @@ export const shortDateOptions: Intl.DateTimeFormatOptions = {
   day: '2-digit',
   year: 'numeric'
 };
-export const millisecsInDay = 86400000;
+export const MILLI_SECS_PER_DAY = 86400000;
+export const DAYS_PER_WEEK = 7;
 if (!Date.prototype.toShortDateString) { //NOSONAR
-  Date.prototype.toShortDateString = function (): string {
+  Date.prototype.toShortDateString = function (this: Date): string {
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const dt: Date = this;
 
@@ -25,11 +26,11 @@ if (!Date.prototype.toShortDateString) { //NOSONAR
 
 if (!Date.prototype.weekOfYear) { //NOSONAR
   // tslint:disable-next-line: space-before-function-paren
-  Date.prototype.weekOfYear = function (): number {
+  Date.prototype.weekOfYear = function (this: Date): number {
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const dt: Date = this;
     const onejan = new Date(dt.getFullYear(), 0, 1);
-    return Math.ceil(((dt.getTime() - onejan.getTime()) / millisecsInDay + onejan.getDay() + 1) / 7);
+    return Math.ceil(((dt.getTime() - onejan.getTime()) / MILLI_SECS_PER_DAY + onejan.getDay() + 1) / DAYS_PER_WEEK);
   };
 }
 export { };
