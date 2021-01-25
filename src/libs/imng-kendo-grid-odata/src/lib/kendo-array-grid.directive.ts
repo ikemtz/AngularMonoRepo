90a4402c5f126a8b5e6d79b8ff41da2b4d4f0060
IkeMtz <ikemtz@outlook.com>
2021-01-25 13:32:04 -0500

    Fixing bug in array grid pagination

--- src/libs/imng-kendo-grid-odata/src/lib/kendo-array-grid.directive.ts
+++ src/libs/imng-kendo-grid-odata/src/lib/kendo-array-grid.directive.ts
@@ -25,11 +25,11 @@ export class ImngArrayGridDirective implements OnInit, AfterViewInit, OnDestroy 
       mode: 'multiple',
     };
     this.gridComponent.navigable = true;
-    this.gridComponent.pageable = this.pageable || {
+    this.gridComponent.pageable = this.pageable === undefined ? {
       info: true,
       type: 'numeric',
       pageSizes: [5, 10, 20, 50, 100],
-    };
+    } : this.pageable;
     this.subscriptions.push(
       this.gridComponent.dataStateChange.subscribe((t: ODataGridStateChangeEvent) => {
         this.gridComponent.sort = this.arrayComponent.state.sort = t.sort;
