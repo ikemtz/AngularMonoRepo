ef7ddc3dd5f228ace58bb01ccc75c566f3a87f87
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2021-04-06 22:55:11 -0400

    Code smell cleanup (#233)
    * First batch of code smell cleanups
    
    * Reverting back changes to auth-guard.ts
    
    * Fixing issues with numerical values
    
    * fixing build

--- src/libs/imng-nrsrx-client-utils/src/index.ts
+++ src/libs/imng-nrsrx-client-utils/src/index.ts
@@ -5,3 +5,4 @@ export * from './lib/date.extensions';
 export * from './lib/array.extensions';
 export * from './lib/normalize-request';
 export * from './lib/value-type';
+export * from './lib/is-a';
