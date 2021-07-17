df219be65319af0561de18e6aaed423998ee8468
IkeMtz <ikemtz@outlook.com>
2021-07-16 21:36:07 -0400

    Removing TSLINT dependency from schematics

--- schematics/src/imng-crud/index.ts
+++ schematics/src/imng-crud/index.ts
@@ -1,8 +1,8 @@
 import { Rule, SchematicContext, Tree, chain } from '@angular-devkit/schematics';
-import { getSwaggerDoc, generateFiles, runLint, IOptions } from '../shared'; 
+import { getSwaggerDoc, generateFiles, IOptions } from '../shared'; 
 
 export function imngCrud(_options: IOptions): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return chain([getSwaggerDoc(_options), generateFiles(_options, 'crud'), runLint(_options)])(tree, _context);
+    return chain([getSwaggerDoc(_options), generateFiles(_options, 'crud')])(tree, _context);
   };
 }
