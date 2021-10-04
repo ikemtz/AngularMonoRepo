524025670ce3f0ea7fec9d54ea4edeaef380da2e
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2021-10-04 17:14:25 -0400

    Child column template bug fix and code smells (#304)
    * Initial attempt
    
    * Adding empty row filtering
    
    * Fixing typo
    
    * Addressing ChangeDetection Bug
    
    * Addressing code smells and bug fix

--- src/libs/imng-kendo-grid-filtering/src/lib/multi-select-filter/multi-select-filter.component.ts
+++ src/libs/imng-kendo-grid-filtering/src/lib/multi-select-filter/multi-select-filter.component.ts
@@ -1,91 +1,89 @@
 import { Component, AfterViewInit, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { FilterService } from "@progress/kendo-angular-grid";
+import { FilterService } from '@progress/kendo-angular-grid';
 import { CompositeFilterDescriptor, distinct, filterBy, FilterDescriptor } from '@progress/kendo-data-query';
 import { ODataState } from 'imng-kendo-odata';
 
 @Component({
   selector: 'imng-multi-select-filter',
   template: `
-<ul>
-  <li *ngIf="showFilter">
-    <input class="k-textbox" (input)="onInput($event)" />
-  </li>
-  <li
-    #itemElement
-    *ngFor="let item of currentData; let i = index"
-    (click)="onSelectionChange(valueAccessor(item), itemElement)"
-    [ngClass]="{ 'k-state-selected': isItemSelected(item) }"
-  >
-    <input
-      type="checkbox"
-      id="chk-{{ valueAccessor(item) }}"
-      (focus)="onFocus(itemElement)"
-      class="k-checkbox"
-      [checked]="isItemSelected(item)"
-    />
-    <label class="k-multiselect-checkbox k-checkbox-label" for="chk-{{ valueAccessor(item) }}">
-      {{ textAccessor(item) }}
-    </label>
-  </li>
-</ul>
-`,
-  styles: [`
-ul {
-  list-style-type: none;
-  height: 200px;
-  overflow-y: scroll;
-  padding-left: 0;
-  padding-right: 12px;
-}
+    <ul>
+      <li *ngIf="showFilter">
+        <input class="k-textbox" (input)="onInput($event)" />
+      </li>
+      <li
+        #itemElement
+        *ngFor="let item of currentData; let i = index"
+        (click)="onSelectionChange(valueAccessor(item), itemElement)"
+        [ngClass]="{ 'k-state-selected': isItemSelected(item) }"
+      >
+        <input
+          type="checkbox"
+          id="chk-{{ valueAccessor(item) }}"
+          (focus)="onFocus(itemElement)"
+          class="k-checkbox"
+          [checked]="isItemSelected(item)"
+        />
+        <label class="k-multiselect-checkbox k-checkbox-label" for="chk-{{ valueAccessor(item) }}">
+          {{ textAccessor(item) }}
+        </label>
+      </li>
+    </ul>
+  `,
+  styles: [
+    `
+      ul {
+        list-style-type: none;
+        height: 200px;
+        overflow-y: scroll;
+        padding-left: 0;
+        padding-right: 12px;
+      }
 
-ul > li {
-  padding: 8px 12px;
-  border: 1px solid rgba(0, 0, 0, 0.08);
-  border-bottom: none;
-}
+      ul > li {
+        padding: 8px 12px;
+        border: 1px solid rgba(0, 0, 0, 0.08);
+        border-bottom: none;
+      }
 
-ul > li:last-of-type {
-  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
-}
+      ul > li:last-of-type {
+        border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+      }
 
-.k-multiselect-checkbox {
-  pointer-events: none;
-}
-  `],
+      .k-multiselect-checkbox {
+        pointer-events: none;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MultiSelectFilterComponent implements AfterViewInit {
-
   @Input() public isPrimitive = false;
   @Input() public odataState: ODataState;
   @Input() public data: unknown[];
   @Input() public textField: string = undefined;
   @Input() public valueField: string = undefined;
-  @Input() public field = "";
+  @Input() public field = '';
 
   public currentData: unknown[];
   public showFilter = true;
   public value: unknown[] = [];
 
-  public textAccessor = (dataItem: unknown) =>
-    this.isPrimitive ? dataItem : dataItem[this.textField];
-  public valueAccessor = (dataItem: unknown) =>
-    this.isPrimitive ? dataItem : dataItem[this.valueField];
+  public textAccessor = (dataItem: unknown) => (this.isPrimitive ? dataItem : dataItem[this.textField]);
+  public valueAccessor = (dataItem: unknown) => (this.isPrimitive ? dataItem : dataItem[this.valueField]);
 
-  constructor(private readonly filterService: FilterService, private readonly changeDetectorRef: ChangeDetectorRef) {
-  }
+  constructor(private readonly filterService: FilterService, private readonly changeDetectorRef: ChangeDetectorRef) { }
 
   public ngAfterViewInit() {
     this.currentData = this.data;
-    const tempValue = this.odataState.filter?.filters.map(
-      (f: CompositeFilterDescriptor) => f.filters
-        .filter((x: FilterDescriptor) => x.field === this.field)
-        .map((x: FilterDescriptor) => x.value)
+    const tempValue = this.odataState.filter?.filters.map((f: CompositeFilterDescriptor) =>
+      f.filters.filter((x: FilterDescriptor) => x.field === this.field).map((x: FilterDescriptor) => x.value),
     );
-    this.value = tempValue?.length >= 0 ? tempValue.reduce((previousArray: [], currentArray: []) => previousArray.concat(...currentArray)) : [];
+    this.value =
+      tempValue?.length >= 0
+        ? tempValue.reduce((previousArray: [], currentArray: []) => previousArray.concat(...currentArray))
+        : [];
 
-    this.showFilter =
-      typeof this.textAccessor(this.currentData[0]) === "string";
+    this.showFilter = typeof this.textAccessor(this.currentData[0]) === 'string';
     this.changeDetectorRef.markForCheck();
   }
 
@@ -101,46 +99,57 @@ export class MultiSelectFilterComponent implements AfterViewInit {
     }
     const currentFilter: CompositeFilterDescriptor = {
       logic: this.odataState.filter?.logic ?? 'and',
-      filters: this.odataState.filter?.filters ? this.odataState.filter.filters : []
+      filters: this.odataState.filter?.filters ? this.odataState.filter.filters : [],
     };
-    if (currentFilter.filters.some((x: CompositeFilterDescriptor) => x.filters.some((f: FilterDescriptor) => f.field === this.field))) {
-      currentFilter.filters = currentFilter.filters.filter((x: CompositeFilterDescriptor) => !x.filters.some((f: FilterDescriptor) => f.field === this.field));
+    if (
+      currentFilter.filters.some((x: CompositeFilterDescriptor) =>
+        x.filters.some((f: FilterDescriptor) => f.field === this.field),
+      )
+    ) {
+      currentFilter.filters = currentFilter.filters.filter(
+        (x: CompositeFilterDescriptor) => !x.filters.some((f: FilterDescriptor) => f.field === this.field),
+      );
     }
     if (this.value.length > 0) {
-      currentFilter.filters = [...currentFilter.filters, {
-        logic: 'or', filters: this.value.map((value) => ({
-          field: this.field,
-          operator: "eq",
-          value,
-        }))
-      }];
+      currentFilter.filters = [
+        ...currentFilter.filters,
+        {
+          logic: 'or',
+          filters: this.value.map((value) => ({
+            field: this.field,
+            operator: 'eq',
+            value,
+          })),
+        },
+      ];
     }
     this.filterService.filter(currentFilter);
 
     this.onFocus(li);
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public onInput(e: { target: { value: string | number | Date; }; } | any) {
     this.currentData = distinct(
       [
         ...this.currentData.filter((dataItem: unknown) =>
-          this.value.some((val) => val === this.valueAccessor(dataItem))
+          this.value.some((val) => val === this.valueAccessor(dataItem)),
         ),
         ...filterBy(this.data, {
-          operator: "contains",
+          operator: 'contains',
           field: this.textField,
           value: e.target.value,
         }),
       ],
-      this.textField
+      this.textField,
     );
     this.changeDetectorRef.markForCheck();
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public onFocus(li: any): void {
     const ul = li.parentNode;
-    const below =
-      ul.scrollTop + ul.offsetHeight < li.offsetTop + li.offsetHeight;
+    const below = ul.scrollTop + ul.offsetHeight < li.offsetTop + li.offsetHeight;
     const above = li.offsetTop < ul.scrollTop;
 
     // Scroll to focused checkbox
