48b5cc1fb3b2663d8d1826cd344585069319e481
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2021-10-04 16:05:58 -0400

    Fixing Typo (#302)
    * Initial attempt
    
    * Adding empty row filtering
    
    * Fixing typo

--- src/libs/imng-kendo-grid-odata/src/lib/kendo-odata-grid.module.ts
+++ src/libs/imng-kendo-grid-odata/src/lib/kendo-odata-grid.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImngODataGridDirective } from './kendo-odata-grid.directive';
-import { ImngGridChildColumTemplateComponent } from './kendo-child-colum-template/kendo-child-colum-template.component';
+import { ImngGridChildColumnTemplateComponent } from './kendo-child-column-template/kendo-child-column-template.component';
 
 @NgModule({
-  declarations: [ImngODataGridDirective, ImngGridChildColumTemplateComponent],
+  declarations: [ImngODataGridDirective, ImngGridChildColumnTemplateComponent],
   imports: [CommonModule],
-  exports: [ImngODataGridDirective, ImngGridChildColumTemplateComponent],
+  exports: [ImngODataGridDirective, ImngGridChildColumnTemplateComponent],
 })
 export class ImngKendoGridODataModule { }
