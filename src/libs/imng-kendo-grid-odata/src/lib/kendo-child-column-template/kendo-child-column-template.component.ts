99ecf24c0aa055b12ba1583e39f86d92c932dbba
IkeMtz <ikemtz@outlook.com>
2021-10-04 20:18:11 -0400

    performance bug fixes

--- src/libs/imng-kendo-grid-odata/src/lib/kendo-child-column-template/kendo-child-column-template.component.ts
+++ src/libs/imng-kendo-grid-odata/src/lib/kendo-child-column-template/kendo-child-column-template.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'imng-kendo-grid-child-column-template',
@@ -6,16 +6,17 @@ import { Component, Input, Output, EventEmitter, AfterViewInit, ChangeDetectionS
   styleUrls: ['./kendo-child-column-template.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ImngGridChildColumnTemplateComponent implements AfterViewInit {
+export class ImngGridChildColumnTemplateComponent implements OnInit {
   @Input() public data: unknown[];
   @Input() public field: string;
   @Input() public visibleRecCount = 5;
   @Input() public showMore = true;
+  @Input() public toolTipJoinCharacter = ';';
   @Output() showMoreClicked = new EventEmitter();
 
   public currentData: unknown[];
 
-  public ngAfterViewInit(): void {
+  public ngOnInit(): void {
     this.currentData = (this.data || [])
       .filter(val => (val[this.field] || '').length > 0)
       .map(t => t[this.field]);
@@ -24,7 +25,7 @@ export class ImngGridChildColumnTemplateComponent implements AfterViewInit {
   public formatToolTip(): string {
     return this.currentData
       .filter((_val, index) => index >= this.visibleRecCount)
-      .join(' ; ');
+      .join(`${this.toolTipJoinCharacter} `);
   }
 
   public moreClicked(): void {
