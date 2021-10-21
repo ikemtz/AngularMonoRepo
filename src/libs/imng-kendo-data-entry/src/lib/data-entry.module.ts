34f7d2ec8afb3ee6a60c12b32a769b92e0faf6d3
IkeMtz <ikemtz@outlook.com>
2021-10-20 23:28:55 -0400

    Removing DialogModule again

--- src/libs/imng-kendo-data-entry/src/lib/data-entry.module.ts
+++ src/libs/imng-kendo-data-entry/src/lib/data-entry.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataEntryDialogComponent } from './data-entry-dialog.component';
 import { DialogButtonsDirective } from './dialog-buttons.directive';
-import { DialogModule } from '@progress/kendo-angular-dialog';
+import {
+  DialogContainerDirective,
+  DialogContainerService,
+  DialogService,
+  DIALOG_DIRECTIVES,
+  SharedModule,
+} from '@progress/kendo-angular-dialog';
 
 @NgModule({
-  imports: [CommonModule, DialogModule],
-  declarations: [DataEntryDialogComponent, DialogButtonsDirective],
+  imports: [CommonModule, SharedModule],
+  declarations: [DataEntryDialogComponent, DialogButtonsDirective, DIALOG_DIRECTIVES, DialogContainerDirective],
   exports: [DataEntryDialogComponent, DialogButtonsDirective],
+  providers: [DialogContainerService, DialogService],
 })
 export class ImngDataEntryDialogModule {}
