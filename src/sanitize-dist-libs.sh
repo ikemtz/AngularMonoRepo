01df3a0bede1af55f9c20dc724cbbfc781a1e3a7
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2021-10-26 15:43:45 -0400

    temporary fix for kendo dialog breakage ... again (#329)
    * merging latest
    
    * Expanding error handling on ngrx effects
    
    * Fixing pipelines
    
    * nx migrations
    
    * Updating angular
    
    * Schematics fixes
    
    * ODataState query param date fix
    
    * Deserialization fix for UTC filters on odata query string param
    
    * temporary fix for kendo dialog breakage ... again

--- src/sanitize-dist-libs.sh
+++ src/sanitize-dist-libs.sh
@@ -6,5 +6,5 @@ value='s/\s*"tslib": "[0-9.>=x]*",\s*//g'
 $(sed -i "$value" ./dist/libs/imng-*/package.json)
 
 # Removing  "require('@progress/kendo-angular-dialog')," from factory on imng-kend0-data-entry.umd.jsb
-value='s/require..@progress.kendo.angular.dialog...//g'
-$(sed -i "$value" ./dist/libs/imng-kendo-data-entry/bundles/imng-kendo-data-entry.umd.js)
+#value='s/require..@progress.kendo.angular.dialog...//g'
+#$(sed -i "$value" ./dist/libs/imng-kendo-data-entry/bundles/imng-kendo-data-entry.umd.js)
