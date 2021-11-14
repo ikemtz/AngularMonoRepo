9492fd3c32af448fd841c55b45eca26abde7d2a8
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2021-11-14 18:16:11 -0500

    restructing date helper methods (#343)
    * schematics changes for afterAll()
    
    * updating extensions
    
    * completing feature
    
    * updating component spec to increase code coverage
    
    * code cleanup
    
    * Fixing bug in grid header
    
    * Bug fix on DateHelper methods
    
    * restructing date helper methods

--- src/libs/imng-nrsrx-client-utils/src/lib/date.helper.ts
+++ src/libs/imng-nrsrx-client-utils/src/lib/date.helper.ts
@@ -1,8 +1,14 @@
+import { DAYS_PER_WEEK, MILLI_SECS_PER_DAY, shortDateOptions } from './date.extensions';
+
 export function weekOfYear(dt: Date): number {
-  return dt.weekOfYear();
+  const onejan = new Date(dt.getFullYear(), 0, 1);
+  return Math.ceil(((dt.getTime() - onejan.getTime()) / MILLI_SECS_PER_DAY + onejan.getDay() + 1) / DAYS_PER_WEEK);
 }
 
 // tslint:disable-next-line: space-before-function-paren
 export function toShortDateString(dt: Date): string {
-  return dt.toShortDateString();
+  const locale =
+    navigator.language || (navigator.languages && navigator.languages.length > 0) ? navigator.languages[0] : 'en-US';
+
+  return dt.toLocaleDateString(locale, shortDateOptions);
 }
