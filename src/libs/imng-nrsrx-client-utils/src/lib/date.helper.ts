b2307e28b0d57fb32393e26597cf2feca2fd3e7c
IkeMtz <ikemtz@outlook.com>
2021-11-14 16:04:13 -0500

    Bug fix on DateHelper methods

--- src/libs/imng-nrsrx-client-utils/src/lib/date.helper.ts
+++ src/libs/imng-nrsrx-client-utils/src/lib/date.helper.ts
@@ -1,10 +1,8 @@
-export class DateHelper {
-  public static weekOfYear(dt: Date): number {
-    return dt.weekOfYear();
-  }
+export function weekOfYear(dt: Date): number {
+  return dt.weekOfYear();
+}
 
-  // tslint:disable-next-line: space-before-function-paren
-  public static toShortDateString(dt: Date): string {
-    return dt.toShortDateString();
-  }
+// tslint:disable-next-line: space-before-function-paren
+export function toShortDateString(dt: Date): string {
+  return dt.toShortDateString();
 }
