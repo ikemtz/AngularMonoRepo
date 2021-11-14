86b5402d5e2e906a8a371b01025fe126edc9f952
IkeMtz <ikemtz@outlook.com>
2021-11-14 18:13:30 -0500

    restructing date helper methods

--- src/libs/imng-nrsrx-client-utils/src/lib/date.extensions.ts
+++ src/libs/imng-nrsrx-client-utils/src/lib/date.extensions.ts
@@ -1,3 +1,5 @@
+import { toShortDateString, weekOfYear } from './date.helper';
+
 declare global {
   export interface Date {
     toShortDateString(): string;
@@ -8,30 +10,24 @@ declare global {
 export const shortDateOptions: Intl.DateTimeFormatOptions = {
   month: '2-digit',
   day: '2-digit',
-  year: 'numeric'
+  year: 'numeric',
 };
 export const MILLI_SECS_PER_DAY = 86400000;
 export const MILLI_SECS_PER_SEC = 60000;
 export const DAYS_PER_WEEK = 7;
-if (!Date.prototype.toShortDateString) { //NOSONAR
+if (!Date.prototype.toShortDateString) {
+  //NOSONAR
   Date.prototype.toShortDateString = function (this: Date): string {
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const dt: Date = this;
-
-    const locale = navigator.language ||
-      (navigator.languages && navigator.languages.length > 0) ? navigator.languages[0] : 'en-US';
-
-    return dt.toLocaleDateString(locale, shortDateOptions);
+    return toShortDateString(this);
   };
 }
 
-if (!Date.prototype.weekOfYear) { //NOSONAR
+if (!Date.prototype.weekOfYear) {
+  //NOSONAR
   // tslint:disable-next-line: space-before-function-paren
   Date.prototype.weekOfYear = function (this: Date): number {
     // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const dt: Date = this;
-    const onejan = new Date(dt.getFullYear(), 0, 1);
-    return Math.ceil(((dt.getTime() - onejan.getTime()) / MILLI_SECS_PER_DAY + onejan.getDay() + 1) / DAYS_PER_WEEK);
+    return weekOfYear(this);
   };
 }
-export { };
+export {};
