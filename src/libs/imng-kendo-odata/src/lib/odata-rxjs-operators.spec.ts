b37b222a2ec1b7d08b7e7d0cc3c1045ccd0e86d3
IkeMtz <ikemtz@outlook.com>
2021-12-06 10:53:23 -0500

    bug fix and unit testing

--- src/libs/imng-kendo-odata/src/lib/odata-rxjs-operators.spec.ts
+++ src/libs/imng-kendo-odata/src/lib/odata-rxjs-operators.spec.ts
@@ -8,26 +8,34 @@ describe('RxJs Operators', () => {
     data: [
       { id: 1, subData: [{ val: 'xyz' }, { val: 123 }] },
       { id: 2, subData: [{ val: 'abc' }, { val: 456 }] },
-    ], total: 5000
+    ],
+    total: 5000,
   });
   const emptyGridData$ = of({
-    data: [], total: 0
+    data: [],
+    total: 0,
   });
 
   it('mapToExtDataResult should function', async () => {
-    const result = await readFirst(gridData$.pipe(
-      map(t => ({
-        value: t.data,
-        '@odata.count': t.data.length
-      })),
-      mapToExtDataResult()));
+    const result = await readFirst(
+      gridData$.pipe(
+        map((t) => ({
+          value: t.data,
+          '@odata.count': t.data.length,
+        })),
+        mapToExtDataResult(),
+      ),
+    );
     expect(result).toMatchSnapshot();
   });
 
   it('mapToExtDataResult should handle Arrays', async () => {
-    const result = await readFirst(gridData$.pipe(
-      map(t => t.data),
-      mapToExtDataResult()));
+    const result = await readFirst(
+      gridData$.pipe(
+        map((t) => t.data),
+        mapToExtDataResult(),
+      ),
+    );
     expect(result).toMatchSnapshot();
   });
 
@@ -36,27 +44,38 @@ describe('RxJs Operators', () => {
     expect(result).toMatchSnapshot();
   });
 
-
   it('getSubGridData should function', async () => {
-    const result = await readFirst(gridData$.pipe(getSubGridData(2, x => x.subData)));
+    const result = await readFirst(gridData$.pipe(getSubGridData(2, (x) => x.subData)));
     expect(result).toMatchSnapshot();
   });
 
   it('getSubData should function', async () => {
-    const result = await readFirst(gridData$.pipe(
-      map(t => t.data),
-      getSubData(2, x => x.subData)));
+    const result = await readFirst(
+      gridData$.pipe(
+        map((t) => t.data),
+        getSubData(2, (x) => x.subData),
+      ),
+    );
     expect(result).toMatchSnapshot();
   });
 
-
   it('should be created', async () => {
-    const result = await readFirst(gridData$.pipe(getSubGridData(2, x => x.subData)));
+    const result = await readFirst(gridData$.pipe(getSubGridData(2, (x) => x.subData)));
     expect(result).toMatchSnapshot();
   });
 
   it('should findById', async () => {
     const result = await readFirst(gridData$.pipe(findById(2)));
+    expect(result).toMatchSnapshot();
+  });
+
+  it('findById handle undefined objects', async () => {
+    const result = await readFirst(of(null).pipe(findById(2)));
+    expect(result).toMatchSnapshot();
+  });
+
+  it('findById handle undefined datasets', async () => {
+    const result = await readFirst(of({ data: undefined }).pipe(findById(2)));
     expect(result).toMatchSnapshot();
   });
 
@@ -73,5 +92,14 @@ describe('RxJs Operators', () => {
   it('should empty firstRecord', async () => {
     const result = await readFirst(emptyGridData$.pipe(firstRecord()));
     expect(result).toStrictEqual({});
+  });
+  it('firstRecord handle undefined objects', async () => {
+    const result = await readFirst(of(null).pipe(firstRecord()));
+    expect(result).toMatchSnapshot();
+  });
+
+  it('firstRecord handle undefined datasets', async () => {
+    const result = await readFirst(of({ data: undefined }).pipe(firstRecord()));
+    expect(result).toMatchSnapshot();
   });
 });
--- src/libs/imng-kendo-odata/src/lib/odata-rxjs-operators.ts
+++ src/libs/imng-kendo-odata/src/lib/odata-rxjs-operators.ts
@@ -10,21 +10,24 @@ export const mapToExtDataResult = <T>(utcNullableProps: string[] = [], dateNulla
     if (!response) {
       return { data: [], total: 0 };
     }
-    const result = Array.isArray(response) ? {
-      data: response,
-      total: response.length
-    } : {
-      data: response.value,
-      total: response['@odata.count'],
-    } as ODataResult<T>;
+    const result = Array.isArray(response)
+      ? {
+          data: response,
+          total: response.length,
+        }
+      : ({
+          data: response.value,
+          total: response['@odata.count'],
+        } as ODataResult<T>);
     result.data = parseDatesInCollection(result.data, utcNullableProps, dateNullableProps);
     return result;
   });
 
-export const firstRecord = <T>() => map((result: ODataResult<T>) => (result.data.length > 0 ? result.data[0] : {} as T));
+export const firstRecord = <T>() =>
+  map((result: ODataResult<T>) => (result?.data?.length > 0 ? result.data[0] : ({} as T)));
 
-export const findById = <T extends { id?: string | number | Date; }>(id?: string | number | Date) =>
-  map((source: ODataResult<T>) => source.data.find(f => f.id === id) || {} as T);
+export const findById = <T extends { id?: string | number | Date }>(id?: string | number | Date) =>
+  map((source: ODataResult<T>) => source?.data?.find((f) => f.id === id) || ({} as T));
 
 export function parseDatesInCollection<T>(
   collection: Array<T>,
@@ -32,24 +35,24 @@ export function parseDatesInCollection<T>(
   dateNullableProps: string[] = [],
 ): Array<T> {
   if (collection.length > 0) {
-    const utcProps = Object.keys(collection[0]).filter(x => x.endsWith('Utc'));
-    const dateProps = Object.keys(collection[0]).filter(x => x.endsWith('Date'));
+    const utcProps = Object.keys(collection[0]).filter((x) => x.endsWith('Utc'));
+    const dateProps = Object.keys(collection[0]).filter((x) => x.endsWith('Date'));
 
-    utcNullableProps?.forEach(t => {
+    utcNullableProps?.forEach((t) => {
       if (utcProps.indexOf(t) === -1) {
         utcProps.push(t);
       }
     });
 
-    dateNullableProps?.forEach(t => {
+    dateNullableProps?.forEach((t) => {
       if (dateProps.indexOf(t) === -1) {
         dateProps.push(t);
       }
     });
 
     collection.forEach((val: unknown) => {
-      utcProps.filter(p => val[p]).forEach(p => (val[p] = new Date(val[p])));
-      dateProps.filter(p => val[p]).forEach(p => (val[p] = toLocalDate(val[p])));
+      utcProps.filter((p) => val[p]).forEach((p) => (val[p] = new Date(val[p])));
+      dateProps.filter((p) => val[p]).forEach((p) => (val[p] = toLocalDate(val[p])));
     });
   }
   return collection;
@@ -60,34 +63,30 @@ export function toLocalDate(date: string): Date {
   return new Date(dt.getTime() + Math.abs(dt.getTimezoneOffset() * MILLI_SECS_PER_SEC));
 }
 
-export function getSubGridData<
-  PARENT_ENTITY extends { id?: idType; },
-  SUB_ENTITY>(
-    id: idType,
-    mappingFunction: (entity: PARENT_ENTITY) => SUB_ENTITY[]):
-  (source: Observable<ODataResult<PARENT_ENTITY>>) => Observable<SUB_ENTITY[]> {
+export function getSubGridData<PARENT_ENTITY extends { id?: idType }, SUB_ENTITY>(
+  id: idType,
+  mappingFunction: (entity: PARENT_ENTITY) => SUB_ENTITY[],
+): (source: Observable<ODataResult<PARENT_ENTITY>>) => Observable<SUB_ENTITY[]> {
   // tslint:disable-next-line: space-before-function-paren
   return function (source: Observable<ODataResult<PARENT_ENTITY>>): Observable<SUB_ENTITY[]> {
     return source.pipe(
-      map(t => t.data.find(f => f.id === id)),
+      map((t) => t.data.find((f) => f.id === id)),
       map((entity: PARENT_ENTITY) => mappingFunction(entity)),
-      filter((t) => !!t)
+      filter((t) => !!t),
     );
   };
 }
 
-export function getSubData<
-  PARENT_ENTITY extends { id?: idType; },
-  SUB_ENTITY>(
-    id: idType,
-    mappingFunction: (entity: PARENT_ENTITY) => SUB_ENTITY[]):
-  (source: Observable<Array<PARENT_ENTITY>>) => Observable<SUB_ENTITY[]> {
+export function getSubData<PARENT_ENTITY extends { id?: idType }, SUB_ENTITY>(
+  id: idType,
+  mappingFunction: (entity: PARENT_ENTITY) => SUB_ENTITY[],
+): (source: Observable<Array<PARENT_ENTITY>>) => Observable<SUB_ENTITY[]> {
   // tslint:disable-next-line: space-before-function-paren
   return function (source: Observable<Array<PARENT_ENTITY>>): Observable<SUB_ENTITY[]> {
     return source.pipe(
-      map(t => t.find(f => f.id === id)),
+      map((t) => t.find((f) => f.id === id)),
       map((entity: PARENT_ENTITY) => mappingFunction(entity)),
-      filter((t) => !!t)
+      filter((t) => !!t),
     );
   };
 }
