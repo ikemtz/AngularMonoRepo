5524870a57b21f5d4c59abd1bfe545cd15919d32
IkeMtz <ikemtz@outlook.com>
2022-03-02 02:04:20 -0500

    Fixing unit tests

--- schematics/src/shared/find-up.spec.ts
+++ schematics/src/shared/find-up.spec.ts
@@ -2,7 +2,7 @@ import { findUpSync } from "./find-up";
 
 describe(`find-up`, () => {
   test(`should find file`, () => {
-    const file = findUpSync('.gitignore');
-    expect(file).toBe('C:\\Repos\\AngularMonoRepo\\schematics\\.gitignore');
+    const file = findUpSync('README.md');
+    expect(file).toBe('C:\\Repos\\AngularMonoRepo\\schematics\\README.md');
   })
 });
