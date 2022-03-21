c16d29ab0132666fa5ed2f3c111045d357406b34
IkeMtz <ikemtz@outlook.com>
2022-03-20 23:25:48 -0400

    imng-inventory

---
+++ src2/apps/imng-inventory/src/app/app.component.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+
+describe('AppComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
---
+++ src2/apps/imng-inventory/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import {
+  Component,
+  AfterViewInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { BarcodeScannerLivestreamComponent } from 'ngx-barcode-scanner';
+
+@Component({
+  selector: 'imng-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class AppComponent implements AfterViewInit {
+  @ViewChild(BarcodeScannerLivestreamComponent)
+  barecodeScanner: BarcodeScannerLivestreamComponent;
+  public barcodeValue: string;
+  public readonly barcodeTypes = ['upc'];
+
+  public errorMessage: string;
+
+  ngAfterViewInit(): void {
+    this.barecodeScanner.start();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onValueChanges(result: { codeResult: { code: string } } | any): void {
+    this.barcodeValue = result.codeResult.code;
+    console.log(JSON.stringify(result));
+  }
+  onStarted(started: unknown): void {
+    console.log(started);
+  }
+}
