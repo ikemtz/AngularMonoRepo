502b069e0f0e3bca01c493851390aade9bbd4ce6
IkeMtz <ikemtz@outlook.com>
2022-03-30 19:08:20 -0400

    Adding support for negotiation skip on signalr client

--- src/libs/imng-signalr-ngrx/src/lib/services/hub-connection-injector.service.ts
+++ src/libs/imng-signalr-ngrx/src/lib/services/hub-connection-injector.service.ts
@@ -41,8 +41,11 @@ export class HubConnectionInjectorService implements OnDestroy, Subscribable {
 
   public getNewHubConnection(accessToken: string): HubConnection {
     return new HubConnectionBuilder()
-      .withUrl(this.signalrConfiguration.hostUrl, { accessTokenFactory: () => accessToken })
-      .configureLogging(this.signalrConfiguration.logLevel)
+      .withUrl(this.signalrConfiguration.hostUrl, {
+        ...this.signalrConfiguration,
+        accessTokenFactory: () => accessToken,
+      })
+      .configureLogging(this.signalrConfiguration.logger)
       .withAutomaticReconnect()
       .build();
   }
