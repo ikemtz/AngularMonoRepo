19406ce94b8e1045f4afd5f3c3b14e75bf84f3fa
IkeMtz <ikemtz@outlook.com>
2022-04-12 08:50:43 -0400

    Adding code coverage

--- src/jest.preset.js
+++ src/jest.preset.js
@@ -1,3 +1,17 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html', 'cobertura'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '.',
+        outputName: './junit.xml',
+      },
+    ],
+  ],
+};
