66bfe0eec01baa7a2036383bf01e5b080a8cb0a9
IkeMtz <ikemtz@outlook.com>
2022-04-13 13:34:17 -0400

    Updating ngrx-idle to use CreateFeature

--- src/libs/imng-kendo-ngrx-idle/src/lib/+state/idle.reducer.spec.ts
+++ src/libs/imng-kendo-ngrx-idle/src/lib/+state/idle.reducer.spec.ts
@@ -1,12 +1,12 @@
 import { Action } from '@ngrx/store';
-import { featureReducer, initialState } from './idle.reducer';
+import { idleFeature, initialState } from './idle.reducer';
 
 describe('Idle Reducer', () => {
   describe('an unknown action', () => {
     it('should return the previous state', () => {
       const action = {} as Action;
 
-      const result = featureReducer(initialState, action);
+      const result = idleFeature.reducer(initialState, action);
 
       expect(result).toBe(initialState);
     });
