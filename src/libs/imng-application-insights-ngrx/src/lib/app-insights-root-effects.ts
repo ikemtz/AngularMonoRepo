f4161a697012dfa41e9eb6fa001458260ae7ebd2
IkeMtz <ikemtz@outlook.com>
2022-04-15 13:20:45 -0400

    Adding lint issues

--- src/libs/imng-application-insights-ngrx/src/lib/app-insights-root-effects.ts
+++ src/libs/imng-application-insights-ngrx/src/lib/app-insights-root-effects.ts
@@ -9,16 +9,16 @@ abstract class AppInsightsBaseffects {
   constructor(
     protected actions$: Actions,
     protected monitor: AppInsightsMonitoringService
-  ) {}
+  ) { }
   protected readonly noDispatch = { dispatch: false };
 
   protected getTrackLoginPipe(): Observable<Action> {
     return this.actions$.pipe(
       filter(
-        (x: { type: string; payload?: { profile?: { email?: string } } }) =>
+        (x: { type: string; payload?: { profile?: { email?: string; }; }; }) =>
           x.type.toUpperCase().endsWith('USER FOUND') && !!x.payload
       ),
-      tap((x: { type: string; payload?: { profile?: { email?: string } } }) => {
+      tap((x: { type: string; payload?: { profile?: { email?: string; }; }; }) => {
         this.monitor.setAuthenticatedUserContext(
           x.payload?.profile?.email || 'unknown'
         );
@@ -38,7 +38,7 @@ abstract class AppInsightsBaseffects {
   protected getTrackExceptionsPipe(): Observable<Action> {
     return this.actions$.pipe(
       filter((x) => x.type.toUpperCase().endsWith('ERROR')),
-      tap((x: { type: string; payload?: Error }) => {
+      tap((x: { type: string; payload?: Error; }) => {
         this.monitor.logException(x.payload || new Error('Unknown Error'));
       })
     );
@@ -47,24 +47,27 @@ abstract class AppInsightsBaseffects {
 
 @Injectable()
 export class AppInsightsVerboseRootEffects extends AppInsightsBaseffects {
-  trackEvents = createEffect(
-    () =>
-      this.actions$.pipe(
-        tap((x: { type: string; payload?: never }) =>
-          this.monitor.logEvent(x.type, x.payload)
-        )
-      ),
+  trackEvents = createEffect(() => {
+    return this.actions$.pipe(
+      tap((x: { type: string; payload?: never; }) =>
+        this.monitor.logEvent(x.type, x.payload)
+      )
+    );
+  },
     this.noDispatch
   );
 
-  trackLogin = createEffect(() => this.getTrackLoginPipe(), this.noDispatch);
+  trackLogin = createEffect(() => {
+    return this.getTrackLoginPipe();
+  }, this.noDispatch);
 
-  trackLogout = createEffect(() => this.getTrackLogoutPipe(), this.noDispatch);
+  trackLogout = createEffect(() => {
+    return this.getTrackLogoutPipe();
+  }, this.noDispatch);
 
-  trackExceptions = createEffect(
-    () => this.getTrackExceptionsPipe(),
-    this.noDispatch
-  );
+  trackExceptions = createEffect(() => {
+    return this.getTrackExceptionsPipe();
+  }, this.noDispatch);
 
   constructor(actions$: Actions, monitor: AppInsightsMonitoringService) {
     super(actions$, monitor);
@@ -73,17 +76,21 @@ export class AppInsightsVerboseRootEffects extends AppInsightsBaseffects {
 
 @Injectable()
 export class AppInsightsInfoRootEffects extends AppInsightsBaseffects {
-  trackEvents = createEffect(
-    () => this.actions$.pipe(tap((x) => this.monitor.logEvent(x.type))),
-    this.noDispatch
-  );
+  trackEvents = createEffect(() => {
+    return this.actions$.pipe(tap((x) => this.monitor.logEvent(x.type)));
+  }, this.noDispatch);
 
-  trackLogin = createEffect(() => this.getTrackLoginPipe(), this.noDispatch);
+  trackLogin = createEffect(() => {
+    return this.getTrackLoginPipe();
+  }, this.noDispatch);
 
-  trackLogout = createEffect(() => this.getTrackLogoutPipe(), this.noDispatch);
+  trackLogout = createEffect(() => {
+    return this.getTrackLogoutPipe();
+  }, this.noDispatch);
 
-  trackExceptions = createEffect(
-    () => this.getTrackExceptionsPipe(),
+  trackExceptions = createEffect(() => {
+    return this.getTrackExceptionsPipe();
+  },
     this.noDispatch
   );
 
