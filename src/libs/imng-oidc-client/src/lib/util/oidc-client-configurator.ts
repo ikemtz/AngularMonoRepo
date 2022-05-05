efdbc5f4ade4bfc5ac095762a4d91bdf3f045719
IkeMtz <ikemtz@outlook.com>
2022-05-05 11:55:12 -0400

    UserStore expansion

--- src/libs/imng-oidc-client/src/lib/util/oidc-client-configurator.ts
+++ src/libs/imng-oidc-client/src/lib/util/oidc-client-configurator.ts
@@ -1,4 +1,5 @@
 import { isNullOrUndefined } from 'imng-nrsrx-client-utils';
+import { WebStorageStateStore } from 'oidc-client';
 import { OidcClientConfig } from '../models/oidc-client-config';
 import { OidcLibraryConfig } from '../models/oidc-library-config';
 
@@ -21,6 +22,10 @@ export function oidcConfigurator(
           ? true
           : oidcClientConfig.automaticSilentRenew,
       metadataUrl: `${oidcClientConfig.authority}/.well-known/openid-configuration`,
+      userStore: new WebStorageStateStore({
+        store: oidcClientConfig.webStorageStateStore || sessionStorage,
+        prefix: 'imng-oidc'
+      }),
     },
     getUserMetadata: oidcClientConfig.getUserMetadata,
     useCallbackFlag:
