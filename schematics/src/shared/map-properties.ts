c027728d6944388e89cce084e04ef3db8d841942
IkeMtz <ikemtz@outlook.com>
2022-07-11 09:58:51 -0400

    Enum cleanup

---
+++ schematics/src/shared/map-properties.ts
@@ -0,0 +1,38 @@
+import { PropertyInfo, OpenApiComponent, OpenApiDocument } from './open-api-component';
+import { IOptions } from './options';
+import { mapPropertyAttributes } from './rules';
+
+const excludedFields = ['createdBy', 'createdOnUtc', 'tenantId', 'updatedBy', 'updatedOnUtc'];
+
+export function mapProperties(properties: { [key: string]: PropertyInfo; }, options: IOptions, openApiComponent: OpenApiComponent, openApiDoc: OpenApiDocument) {
+  let firstProperty: PropertyInfo | undefined;
+  const filteredProperties: PropertyInfo[] = [];
+  for (const propertyKey in properties) {
+    const originalProperty = properties[propertyKey];
+    if (excludedFields.indexOf(propertyKey) < 0 && originalProperty.type !== 'array') {
+      const property = mapPropertyAttributes(options, originalProperty, {
+        ...properties[propertyKey],
+        name: propertyKey,
+        required: (openApiComponent.required ?? []).indexOf(propertyKey) > -1,
+      });
+
+      mapReferencedProperties(property, propertyKey,openApiComponent, openApiDoc);
+      if (!firstProperty && propertyKey !== 'id') {
+        firstProperty = property;
+      }
+      filteredProperties.push(property);
+    }
+  }
+  return { firstProperty, filteredProperties };
+}
+function mapReferencedProperties(property: PropertyInfo, propertyKey: string, openApiComponent: OpenApiComponent, openApiDoc: OpenApiDocument) {
+  const prop = openApiComponent.properties[propertyKey];
+  const propTypeNames = prop['$ref']?.split('/');
+  if (propTypeNames) {
+    const typeName: string = propTypeNames.pop() || '';
+    const refComponent = openApiDoc.components.schemas[typeName];
+    property.propertyTypeName = `${typeName}${refComponent.enum ? '' : 'Properties'}`;
+    property.enum = refComponent.enum;
+  }
+}
+
