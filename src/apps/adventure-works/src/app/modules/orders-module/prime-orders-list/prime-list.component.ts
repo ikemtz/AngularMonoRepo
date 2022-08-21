8455d7ac413e86f7b8503c657849e951436ab4b5
IkeMtz <ikemtz@outlook.com>
2022-08-21 16:43:14 -0400

    Removing code duplication

--- src/apps/adventure-works/src/app/modules/orders-module/prime-orders-list/prime-list.component.ts
+++ src/apps/adventure-works/src/app/modules/orders-module/prime-orders-list/prime-list.component.ts
@@ -4,7 +4,7 @@ import { DetailExpandEvent } from '@progress/kendo-angular-grid';
 import { ODataState } from 'imng-kendo-odata';
 
 import { PrimeOrderListFacade } from './prime-list.facade';
-import { CustomerProperties, IOrder, OrderProperties, orderStatusTypeValues, shippingTypeValues } from '../../../models/odata';
+import { IOrder, OrderProperties } from '../../../models/odata';
 import { Observable } from 'rxjs';
 import { ImngPrimeODataTableBaseComponent } from 'imng-prime-table-odata';
 
@@ -14,34 +14,16 @@ const initialGridState: ODataState = {
   selectors: [
     OrderProperties.ID,
     OrderProperties.ORDER_ID,
-    OrderProperties.REVISION_NUM,
     OrderProperties.DATE,
-    OrderProperties.DUE_DATE,
     OrderProperties.SHIP_DATE,
     OrderProperties.STATUS_TYPE,
     OrderProperties.IS_ONLINE_ORDER,
     OrderProperties.NUM,
     OrderProperties.PURCHASE_ORDER_NUM,
-    OrderProperties.CUSTOMER_ID,
-    OrderProperties.SHIP_TO_ADDRESS_ID,
-    OrderProperties.BILL_TO_ADDRESS_ID,
-    OrderProperties.SHIPPING_TYPE,
-    OrderProperties.CREDIT_CARD_APPROVAL_CODE,
-    OrderProperties.SUB_TOTAL,
-    OrderProperties.TAX_AMT,
-    OrderProperties.FREIGHT,
-    OrderProperties.TOTAL_DUE,
-    OrderProperties.COMMENT,
-    OrderProperties.CUSTOMER,
-    OrderProperties.SHIP_TO_ADDRESS,
-    OrderProperties.BILL_TO_ADDRESS,
   ],
   sort: [
     { field: OrderProperties.ORDER_ID, dir: 'asc' },
   ],
-  expanders: [
-    { table: OrderProperties.CUSTOMER, selectors: [CustomerProperties.NAME, CustomerProperties.COMPANY_NAME] }
-  ]
 };
 
 @Component({
@@ -52,8 +34,6 @@ const initialGridState: ODataState = {
 })
 export class PrimeOrderListComponent extends ImngPrimeODataTableBaseComponent<IOrder, PrimeOrderListFacade> {
   public readonly props = OrderProperties;
-  public readonly orderStatusTypes = orderStatusTypeValues;
-  public readonly shippingTypes = shippingTypeValues;
   public currentItem: IOrder | undefined;
   public readonly data$: Observable<IOrder[]>;
 
