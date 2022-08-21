ad0e291754f26b64a69cfef386a1e8b24a597caf
IkeMtz <ikemtz@outlook.com>
2022-08-21 10:13:51 -0400

    Working code - need unit tests next

---
+++ src/libs/imng-prime-table-odata/src/lib/prime-odata-table.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ImngPrimeODataTableDirective } from './prime-odata-table.directive';
+
+describe('ImngPrimeODataTableDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ImngPrimeODataTableDirective({}, {});
+    expect(directive).toBeTruthy();
+  });
+});
