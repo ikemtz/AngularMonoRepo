2a7e80fcbcf5379fe623822449f66dc89fd0e7e9
IkeMtz <ikemtz@outlook.com>
2022-08-31 00:01:16 -0400

    Increasing code coverage

---
+++ src/libs/imng-prime-table-odata/src/lib/prime-odata-component-base-observable.spec.ts
@@ -0,0 +1,163 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ImngPrimeODataTableBaseComponent } from './prime-odata-component-base';
+import {
+  ODataTableMockFacade,
+  createODataGridMockFacade,
+} from '../../testing/src';
+import { readFirst } from 'imng-ngrx-utils/testing';
+import { Filter, FilterOperators, ODataQuery } from 'imng-odata-client';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+
+describe('PrimeODataBasedComponent Observable State', () => {
+  let component: PrimeODataTableTestComponent;
+  let fixture: ComponentFixture<PrimeODataTableTestComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PrimeODataTableTestComponent],
+      providers: [
+        {
+          provide: Router,
+          useValue: { navigate: jest.fn(), routerState: { root: {} } },
+        },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrimeODataTableTestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    expect(component.router).toBeTruthy();
+  });
+
+  it('should export to Excel', async () => {
+    const data = await readFirst(component.excelData());
+    expect(data).toMatchSnapshot();
+  });
+
+  it('should reset', async () => {
+    component.gridDataState = {
+      ...component.gridDataState,
+      filter: {
+        logic: 'and',
+        filters: [
+          { field: 'y', operator: FilterOperators.Contains, value: 56 },
+        ],
+      },
+    };
+    component.resetFilters();
+    expect(component.gridDataState).toMatchSnapshot();
+  });
+
+  it('should reload', async () => {
+    component.reloadEntities();
+    expect(component.facade.reloadEntities).toBeCalledTimes(1);
+  });
+
+  it('should handle dataStateChange', async () => {
+    component.dataStateChange({});
+    expect(component.facade.loadEntities).toBeCalledTimes(2);
+  });
+
+  it('should handle getExportFileName', async () => {
+    const result = component.getExportFileName('unit-test');
+    expect(result).toHaveLength(28);
+  });
+
+  it('should handle getRelatedValue', () => {
+    const result = component.getRelatedValue(
+      { subItem: { name: 'unit-test' } },
+      'subItem',
+      'name',
+    );
+    expect(result).toBe('unit-test');
+  });
+
+  it('should handle getRelatedField', () => {
+    const result = component.getRelatedField('subItem', 'name');
+    expect(result).toBe('subItem.name');
+  });
+
+  it('should serialize/deserialize odataQuery filters correctly', () => {
+    const tempDate = new Date();
+    const serializedResult = component.serializeODataQuery({
+      filter: {
+        logic: 'and',
+        filters: [
+          {
+            field: 'xyzDate',
+            operator: FilterOperators.EqualTo,
+            value: tempDate,
+          },
+        ],
+      },
+    });
+    const deserializedResult =
+      component.deserializeODataQuery(serializedResult);
+    expect(tempDate).toEqual(
+      (deserializedResult.filter?.filters[0] as Filter).value,
+    );
+  });
+
+  it('should limit odataQuery sort parameters', () => {
+    const odataQuery: ODataQuery = {
+      orderBy: [
+        { field: 'a', dir: 'asc' },
+        { field: 'b', dir: 'asc' },
+        { field: 'c', dir: 'asc' },
+        { field: 'd', dir: 'asc' },
+        { field: 'e', dir: 'asc' },
+        { field: 'f', dir: 'asc' },
+      ],
+    };
+    component.facade.loadEntities = jest.fn();
+    component.loadEntities(odataQuery);
+    expect(component.facade.loadEntities).toBeCalledTimes(1);
+    expect(component.facade.loadEntities).toBeCalledWith({
+      orderBy: [
+        { field: 'a', dir: 'asc' },
+        { field: 'b', dir: 'asc' },
+        { field: 'c', dir: 'asc' },
+        { field: 'd', dir: 'asc' },
+        { field: 'e', dir: 'asc' },
+      ],
+    });
+  });
+
+  it('should serialize/deserialize odataQuery correctly', () => {
+    const serializedResult = component.serializeODataQuery({});
+    const deserializedResult =
+      component.deserializeODataQuery(serializedResult);
+    expect(deserializedResult).toStrictEqual({});
+  });
+});
+
+const initialGridState: ODataQuery = {
+  select: ['x', 'y', 'z'],
+  orderBy: [{ field: 'x', dir: 'desc' }],
+  filter: {
+    logic: 'and',
+    filters: [{ field: 'x', operator: FilterOperators.EqualTo, value: 1 }],
+  },
+};
+@Component({
+  selector: 'imng-test-component',
+  template: '<h1></h1>',
+})
+// eslint-disable-next-line @typescript-eslint/ban-types
+class PrimeODataTableTestComponent extends ImngPrimeODataTableBaseComponent<
+  object,
+  ODataTableMockFacade
+> {
+  props = {};
+  constructor(router: Router) {
+    super(createODataGridMockFacade(), of(initialGridState), router, of(true));
+  }
+}
