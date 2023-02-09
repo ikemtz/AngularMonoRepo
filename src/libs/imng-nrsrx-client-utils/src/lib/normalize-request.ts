51b6f1fba74fe3e209cf2ba15076717c675ab627
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2023-02-09 13:33:40 -0500

    Adding string trim functionality on normalizeRequest

--- src/libs/imng-nrsrx-client-utils/src/lib/normalize-request.ts
+++ src/libs/imng-nrsrx-client-utils/src/lib/normalize-request.ts
@@ -3,14 +3,19 @@ import { isaString } from './id-type';
  *   Summary: This will update an object to make all properties with empty string values have null values instead.
  * @usageNotes
  * Only use this method with plain objects.
-**/
+ **/
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function normalizeRequest<T extends object>(payload: T): T {
   for (const x in payload) {
     const val: unknown = payload[x];
-    if ((isaString(val) && !val.length) ||
-      (Array.isArray(val) && !val.length)) {
+    if (isaString(val)) {
+      val?.trim();
+    }
+    if (
+      (isaString(val) && !val.length) ||
+      (Array.isArray(val) && !val.length)
+    ) {
       payload[x] = null as never;
     }
   }
