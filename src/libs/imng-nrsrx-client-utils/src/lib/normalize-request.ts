b7fe1401c0bef66068808f8f2dadfd2df2a3c1b7
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2023-02-09 17:34:23 -0500

    Expanding normalize request functionality to trim strings (#513)
    * Fixing form validation issues imng-kendo-data-entry
    
    * Fixing imng-kendo-data-entry
    
    * Fixing unit tests
    
    * Fixing lint issue
    
    * Making formId dynamic in data entry
    
    * updating snapshots
    
    * Updating npm package #
    
    * Adding back saveButtonText on data-entry
    
    * Fixing data-entry tests
    
    * Some null reference fixes found during testing
    
    * Adding string trim functionality on normalizeRequest
    
    * expanding normalize request to trim strings

--- src/libs/imng-nrsrx-client-utils/src/lib/normalize-request.ts
+++ src/libs/imng-nrsrx-client-utils/src/lib/normalize-request.ts
@@ -3,14 +3,19 @@ import { isaString } from './id-type';
  *   Summary: This will update an object to make all properties with empty string values have null values instead.
  * @usageNotes
  * Only use this method with plain objects.
-**/
+ **/
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function normalizeRequest<T extends object>(payload: T): T {
   for (const x in payload) {
-    const val: unknown = payload[x];
-    if ((isaString(val) && !val.length) ||
-      (Array.isArray(val) && !val.length)) {
+    let val: unknown = payload[x];
+    if (isaString(val)) {
+      payload[x] = val = val?.trim() as never;
+    }
+    if (
+      (isaString(val) && !val.length) ||
+      (Array.isArray(val) && !val.length)
+    ) {
       payload[x] = null as never;
     }
   }
