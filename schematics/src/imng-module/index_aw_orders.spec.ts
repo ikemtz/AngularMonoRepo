450ba8ca0d6ffb6a8c1a299717b6daf2e77f2917
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2023-05-20 14:15:59 -0400

    NPM package updates on schematics (#522)
    * Adding client_secret to OidcClientConfig
    
    * Updating snapshots
    
    * updating snapshots and dependencies
    
    * Updating package.lock
    
    * Updating peer dependencies
    
    * Updating peer dependency requirements
    
    * Updating Jest snapshots
    
    * Upgrading to angular 16
    
    * Updating snapshot
    
    * NPM package updates
    
    * removing npm-force-resolutions
    
    * Removing .toPromise()
    
    * Fixing code coverage

--- schematics/src/imng-module/index_aw_orders.spec.ts
+++ schematics/src/imng-module/index_aw_orders.spec.ts
@@ -5,8 +5,7 @@ import {
 } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 import { IOptions } from '../shared';
-import * as pluralize from 'pluralize';
-import { readFirst } from '@nrwl/angular/testing/src/testing-utils';
+import * as pluralize from 'pluralize'; 
 import { classify, dasherize } from '@angular-devkit/core/src/utils/strings';
 
 const collectionPath = path.join(__dirname, `../collection.json`);
@@ -23,9 +22,7 @@ describe(`imng-module AdventureWorks Orders`, () => {
       swaggerProperties: [],
       appPrefix: 'aw',
     };
-    tree = await readFirst(
-      runner.runSchematicAsync(`imng-module`, options, Tree.empty())
-    );
+    tree = await runner.runSchematic(`imng-module`, options, Tree.empty());
   })
   test(`tree files should match`, () => {
     expect(tree.files).toMatchSnapshot();
