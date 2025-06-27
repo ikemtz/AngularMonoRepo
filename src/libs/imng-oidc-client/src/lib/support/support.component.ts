ceded01c729072b0653750751971c8bcfffac50c
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2025-06-27 07:27:23 -0400

    Control Flow Migration

--- src/libs/imng-oidc-client/src/lib/support/support.component.ts
+++ src/libs/imng-oidc-client/src/lib/support/support.component.ts
@@ -7,34 +7,44 @@ import { map } from 'rxjs/operators';
 @Component({
     selector: 'imng-support',
     template: `<div class="container pt-5 mt-5">
-    <div class="row h3 text-center">
-      <div class="col-md-12 text-center">OIDC Support</div>
-    </div>
-    <div class="row">
-      <div class="col-md-3">Audiences</div>
-      <div class="col-md-8">
-        <span *ngFor="let item of facade.audiences$ | async; last as isLast">
-          {{ item }} <br *ngIf="!isLast" />
-        </span>
+      <div class="row h3 text-center">
+        <div class="col-md-12 text-center">OIDC Support</div>
+      </div>
+      <div class="row">
+        <div class="col-md-3">Audiences</div>
+        <div class="col-md-8">
+          @for (item of facade.audiences$ | async; track item; let isLast = $last) {
+            <span>
+              {{ item }} @if (!isLast) {
+              <br />
+            }
+          </span>
+        }
       </div>
     </div>
     <div class="row">
       <div class="col-md-3">Permissions</div>
       <div class="col-md-8">
-        <span *ngFor="let item of facade.permissions$ | async; last as isLast">
-          {{ item }} <br *ngIf="!isLast" />
+        @for (item of facade.permissions$ | async; track item; let isLast = $last) {
+          <span>
+            {{ item }} @if (!isLast) {
+            <br />
+          }
         </span>
-      </div>
+      }
+    </div>
     </div>
     <div class="row">
       <div class="col-md-3">Expires</div>
       <div class="col-md-8">{{ facade.expiresAt$ | async }}</div>
     </div>
-    <div class="row" *ngFor="let item of profileValue$ | async">
-      <div class="col-md-3">{{ item.key }}</div>
-      <div class="col-md-8">{{ item.value }}</div>
-    </div>
-  </div> `,
+    @for (item of profileValue$ | async; track item) {
+      <div class="row">
+        <div class="col-md-3">{{ item.key }}</div>
+        <div class="col-md-8">{{ item.value }}</div>
+      </div>
+    }
+    </div>`,
     standalone: false
 })
 export class SupportComponent {
