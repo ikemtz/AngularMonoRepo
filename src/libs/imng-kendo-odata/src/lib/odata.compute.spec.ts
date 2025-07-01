cd76043b2f6fcd31481830944e27351f53e66ff2
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2025-07-01 07:23:04 -0400

    Fixing unit tests

--- src/libs/imng-kendo-odata/src/lib/odata.compute.spec.ts
+++ src/libs/imng-kendo-odata/src/lib/odata.compute.spec.ts
@@ -1,16 +1,37 @@
+import { waitForAsync, TestBed } from '@angular/core/testing';
 import { ODataState } from './odata-state';
 import { ODataService } from './odata.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('compute operations', () => {
+  let odataService: ODataService;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: { get: jest.fn() },
+        },
+        ODataService,
+      ],
+    }).compileComponents();
+  }));
+  beforeEach(() => {
+    odataService = TestBed.inject(ODataService);
+  });
   it('should handle multiplication', () => {
     const gridState: ODataState = {
       selectors: ['id', 'name'],
-      expanders: [{table:'childTable2'}, { table: 'childTable1', selectors: ['id', 'name'] }],
+      expanders: [
+        { table: 'childTable2' },
+        { table: 'childTable1', selectors: ['id', 'name'] },
+      ],
       compute: [{ fieldA: 'x', fieldB: 1000, operator: 'mul', alias: 'z' }],
     };
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const svc = new ODataService(null as any);
-    const result = svc.getODataString(gridState);
+
+    const result = odataService.getODataString(gridState);
 
     expect(result).toStrictEqual(
       '$compute=x mul 1000 as z&$expand=childTable2,childTable1($select=id,name)&$select=id,name',
