40363ecd28dd0039dd01cd4e6e4134c9b1e7dd55
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2026-02-21 08:49:09 -0500

    Finishing updates to schematics

---
+++ src/libs/imng-kendo-testing-stubs/src/index.ts
@@ -0,0 +1,28 @@
+import { IMNG_KENDO_GRID_COLUMN_STUB } from './lib/kendo-grid-column-stub.component';
+import { IMNG_KENDO_GRID_EXCEL_STUB } from './lib/kendo-grid-excel-stub.component';
+import { IMNG_KENDO_GRID_PDF_MARGIN_STUB } from './lib/kendo-grid-pdf-margin-stub.component';
+import { IMNG_KENDO_GRID_PDF_STUB } from './lib/kendo-grid-pdf-stub.component';
+import { IMNG_KENDO_GRID_STUB } from './lib/kendo-grid-stub.component';
+
+export { IMNG_KENDO_GRID_STUB } from './lib/kendo-grid-stub.component';
+export { IMNG_KENDO_GRID_COLUMN_CHOOSER_STUB } from './lib/kendo-grid-column-chooser-stub.component';
+export { IMNG_KENDO_PROGRESSBAR_STUB } from './lib/kendo-progress-bar-stub.component';
+export { IMNG_KENDO_POPUP_STUB } from './lib/kendo-popup-stub.component';
+export { IMNG_KENDO_SPLIT_BUTTON_STUB } from './lib/kendo-split-button-stub.component';
+export { IMNG_KENDO_MENU_STUB } from './lib/kendo-menu-stub.component';
+export { IMNG_KENDO_GRID_EXCEL_STUB } from './lib/kendo-grid-excel-stub.component';
+export { IMNG_KENDO_GRID_PDF_STUB } from './lib/kendo-grid-pdf-stub.component';
+export { IMNG_KENDO_GRID_COLUMN_STUB } from './lib/kendo-grid-column-stub.component';
+export { IMNG_KENDO_GRID_PDF_MARGIN_STUB } from './lib/kendo-grid-pdf-margin-stub.component';
+export { IMNG_KENDO_MULTICOLUMNCOMBOBOX_STUB } from './lib/kendo-multicolumncombobox-stub.component';
+export { IMNG_KENDO_COMBOBOX_COLUMN_STUB } from './lib/kendo-combobox-column-stub.component';
+export { IMNG_KENDO_DATEPICKER_STUB } from './lib/kendo-datepicker-stub.component';
+export { provideGridComponent } from './lib/provide-grid-component';
+
+export const IMNG_KENDO_GRID_TESTING_STUBS = [
+  IMNG_KENDO_GRID_STUB,
+  IMNG_KENDO_GRID_EXCEL_STUB,
+  IMNG_KENDO_GRID_PDF_STUB,
+  IMNG_KENDO_GRID_COLUMN_STUB,
+  IMNG_KENDO_GRID_PDF_MARGIN_STUB,
+];
---
+++ src/libs/imng-kendo-testing-stubs/src/lib/kendo-grid-column-chooser-stub.component.ts
@@ -0,0 +1,19 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  Input,
+  NO_ERRORS_SCHEMA,
+} from '@angular/core';
+
+@Component({
+  selector: 'kendo-grid-column-chooser',
+  template: '',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+})
+export class IMNG_KENDO_GRID_COLUMN_CHOOSER_STUB {
+  @Input() public allowHideAll: boolean | undefined;
+  @Input() public autoSync: boolean | undefined;
+}
