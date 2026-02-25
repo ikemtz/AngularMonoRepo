91237815b70a7e5f542a89fc088e3701108171dc
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2026-02-25 11:50:29 -0500

    Bug fix  schematics standalone list cleanup (#680)
    * fix: update component override in list component spec template
    
    * Updating snapshots to support URL snapshot caching
    
    * Fixing lint errors
    
    * Updating spec file
    
    ---------
    
    Co-authored-by: Isaac Martinez <isaac.1.martinez@lmco.com>

--- schematics/src/imng-module/index_hlti.spec.ts
+++ schematics/src/imng-module/index_hlti.spec.ts
@@ -1,53 +1,55 @@
-import { describe, expect, test } from '@jest/globals';
-import { Tree } from '@angular-devkit/schematics';
+import { describe, expect, test } from "@jest/globals";
+import { Tree } from "@angular-devkit/schematics";
 import {
   SchematicTestRunner,
   UnitTestTree,
-} from '@angular-devkit/schematics/testing';
-import * as path from 'node:path';
-import { IOptions } from '../shared';
-import { plural } from 'pluralize';
-import { classify, dasherize } from '@angular-devkit/core/src/utils/strings';
+} from "@angular-devkit/schematics/testing";
+import * as path from "node:path";
+import { IOptions } from "../shared";
+import { plural } from "pluralize";
+import { classify, dasherize } from "@angular-devkit/core/src/utils/strings";
 
-const collectionPath = path.join(__dirname, '../collection.json');
+const collectionPath = path.join(__dirname, "../collection.json");
 
-describe('imng-module', () => {
-  test('generation works - Health Items', async () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
+describe("imng-module", () => {
+  test("generation works - Health Items", async () => {
+    const runner = new SchematicTestRunner("schematics", collectionPath);
     const options: IOptions = {
-      name: 'healthItem',
+      name: "healthItem",
       openApiJsonUrl:
-        'https://raw.githubusercontent.com/ikemtz/AngularMonoRepo/master/schematics/open-api-docs/nrcrn-hlth-odata.json',
-      path: './test',
+        "https://raw.githubusercontent.com/ikemtz/AngularMonoRepo/master/schematics/open-api-docs/nrcrn-hlth-odata.json",
+      path: "./test",
       swaggerProperties: [],
-      storeName: 'healthItems',
-      appPrefix: 'nrcrn',
+      storeName: "healthItems",
+      appPrefix: "nrcrn",
     };
-    const tree: UnitTestTree = await runner.runSchematic('imng-module', options, Tree.empty());
+    const tree: UnitTestTree = await runner.runSchematic(
+      "imng-module",
+      options,
+      Tree.empty(),
+    );
 
     expect(tree.files.sort()).toMatchSnapshot();
     const effectsFile = tree.get(
       `/test/${plural(dasherize(options.name))}-module/+state/${dasherize(
-        options.name
-      )}-crud.effects.ts`
+        options.name,
+      )}-crud.effects.ts`,
     );
     let content = effectsFile?.content.toString();
     expect(content).toContain(
       `import { ${classify(options.name)}ApiService } from '../${dasherize(
-        plural(options.name)
-      )}-crud/api.service';`
+        plural(options.name),
+      )}-crud/api.service';`,
     );
 
     const listFacadeSpecFile = tree.get(
       `/test/${plural(dasherize(options.name))}-module/${dasherize(
-        plural(options.name)
-      )}-list/list.facade.spec.ts`
+        plural(options.name),
+      )}-list/list.facade.spec.ts`,
     );
     content = listFacadeSpecFile?.content.toString();
     expect(content).toContain(
-      `expect(httpClient.get).toHaveBeenCalledWith('${dasherize(
-        plural(options.name)
-      )}-odata/odata/v1/${classify(plural(options.name))}?&$count=true');`
+      `expect((httpClient.get as jest.Mock).mock.calls[0][0]).toMatchSnapshot();`,
     );
   });
 });
