e74d991cd58785fadd46fa83175b073e32a2011f
Isaac Martinez <isaac.1.martinez@lmco.com>
2026-03-08 00:09:52 -0500

    feat: enhance kendo combobox stub with additional inputs and event emitters

--- src/libs/imng-kendo-testing-stubs/src/lib/kendo-combobox-stub.component.ts
+++ src/libs/imng-kendo-testing-stubs/src/lib/kendo-combobox-stub.component.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { jest } from '@jest/globals';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'kendo-combobox',
@@ -28,14 +29,49 @@ import { jest } from '@jest/globals';
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class IMNG_KENDO_COMBOBOX_STUB {
-  @Input() public valuePrimitive = false;
-  @Input() public valueField = 'valueField';
-  @Input() public textField = 'textField';
-  @Input() public data = [];
+  @Input() public adaptiveMode = 'none';
+  @Input() public adaptiveSubtitle: string | undefined;
+  @Input() public adaptiveTitle = '';
+  @Input() public allowCustom = false;
+  @Input() public clearButton = true;
+  @Input() public data: unknown = [];
   @Input() public disabled = false;
-  @Input() public filterable = true;
-
+  @Input() public fillMode = undefined;
+  @Input() public filterable: undefined | boolean;
+  @Input() public inputAttributes: undefined | { [key: string]: string };
+  @Input() public itemDisabled: undefined | (() => boolean);
+  @Input() public listHeight: undefined | number;
+  @Input() public loading: undefined | boolean;
+  @Input() public placeholder: undefined | string;
+  @Input() public popupSettings: unknown;
+  @Input() public readonly = false;
+  @Input() public rounded: unknown;
+  @Input() public showStickyHeader = true;
+  @Input() public size: unknown;
+  @Input() public suggest = false;
+  @Input() public tabindex = 0;
+  @Input() public textField: string | undefined = 'textField';
+  @Input() public value: unknown;
+  @Input() public valueField: string | undefined = 'valueField';
+  @Input() public valueNormalizer:
+    | undefined
+    | ((text: Observable<string>) => Observable<never>);
+  @Input() public valuePrimitive: undefined | boolean;
+  @Input() public virtual: unknown;
+  public isOpen = false;
+  public close = new EventEmitter<never>();
+  public closed = new EventEmitter<never>();
   public filterChange = new EventEmitter<never>();
+  public inputBlur = new EventEmitter<never>();
+  public inputFocus = new EventEmitter<never>();
+  public blur = new EventEmitter<never>();
+  public focus = new EventEmitter<never>();
+  public open = new EventEmitter<never>();
+  public opened = new EventEmitter<never>();
   public selectionChange = new EventEmitter<never>();
   public valueChange = new EventEmitter<never>();
+
+  public focustItemAt = jest.fn();
+  public reset = jest.fn();
+  public toggle = jest.fn();
 }
