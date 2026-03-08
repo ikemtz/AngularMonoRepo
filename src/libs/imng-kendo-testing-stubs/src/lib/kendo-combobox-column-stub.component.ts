d2cd738a00d9b4419e885210e9e07cc168504614
Isaac Martinez <isaac.1.martinez@lmco.com>
2026-03-08 00:39:51 -0500

    feat: add additional input properties to kendo combobox column stub

--- src/libs/imng-kendo-testing-stubs/src/lib/kendo-combobox-column-stub.component.ts
+++ src/libs/imng-kendo-testing-stubs/src/lib/kendo-combobox-column-stub.component.ts
@@ -6,6 +6,13 @@ import {
   Input,
 } from '@angular/core';
 
+type CssClassValue =
+  | undefined
+  | string
+  | string[]
+  | Set<string>
+  | { [key: string]: never };
+type StyleValue = undefined | { [key: string]: string };
 @Component({
   selector: 'kendo-combobox-column',
   template: '',
@@ -14,5 +21,13 @@ import {
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class IMNG_KENDO_COMBOBOX_COLUMN_STUB {
-  @Input() width = 50;
+  @Input() class: CssClassValue;
+  @Input() field: undefined | string;
+  @Input() headerClass: CssClassValue;
+  @Input() headerStyle: StyleValue;
+  @Input() hidden = false;
+  @Input() media: undefined | string;
+  @Input() style: StyleValue;
+  @Input() title: undefined | string;
+  @Input() width: undefined | number;
 }
